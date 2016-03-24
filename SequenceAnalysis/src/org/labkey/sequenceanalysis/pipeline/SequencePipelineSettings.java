d7ecb064e42d9776ee8f68c91776bbb1ff96469b
Susan Hert <susanh@labkey.com>
2016-03-24 15:48:19 +0000

    merge from modules16.1 through 43042
    SVN r43044 |2016-03-24 15:48:19 +0000

--- SequenceAnalysis/src/org/labkey/sequenceanalysis/pipeline/SequencePipelineSettings.java
+++ SequenceAnalysis/src/org/labkey/sequenceanalysis/pipeline/SequencePipelineSettings.java
@@ -244,7 +244,7 @@ public class SequencePipelineSettings
 
         if (job != null)
         {
-            job.getLogger().error("unable to find file: " + json.toString());
+            job.getLogger().error("unable to find file: " + json.toString(), new Exception());
             job.getLogger().debug("input files were: ");
             for (File f : job.getJobSupport(FileAnalysisJobSupport.class).getInputFiles())
             {
