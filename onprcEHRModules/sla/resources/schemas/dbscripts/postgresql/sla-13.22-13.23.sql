6712b3cc5414d5efca643fdc7efb7db368728f2e
Josh Eckels <jeckels@labkey.com>
2016-04-06 20:29:12 +0000

    Merge onprc16.1Prod revision 43243
    SVN r43267 |2016-04-06 20:29:12 +0000

--- onprcEHRModules/sla/resources/schemas/dbscripts/postgresql/sla-13.22-13.23.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE sla.census DROP CONSTRAINT PK_Census;
-
-ALTER TABLE sla.census ALTER COLUMN objectid SET NOT NULL;
-
-ALTER TABLE sla.census DROP COLUMN rowid;
-
-ALTER TABLE sla.census ADD CONSTRAINT PK_Census PRIMARY KEY (objectid);
--- onprcEHRModules/sla/resources/schemas/dbscripts/postgresql/sla-13.25-13.26.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE sla.census add taskid entityid;
-ALTER TABLE sla.census add formSort integer;
