65c238e869cca86c5b1bae7e4a052c5b89b469a9
Binal Patel <binalp@labkey.com>
2016-09-22 22:40:28 +0000

    Spec 27473 : ETLs - Work order : Modified lineNum column from float to int.
    SVN r46546 |2016-09-22 22:40:28 +0000

--- cnprcEHRModules/cnprc_billing/src/org/labkey/cnprc_billing/CNPRC_BillingModule.java
+++ cnprcEHRModules/cnprc_billing/src/org/labkey/cnprc_billing/CNPRC_BillingModule.java
@@ -40,7 +40,7 @@ public class CNPRC_BillingModule extends ExtendedSimpleModule
     @Override
     public double getVersion()
     {
-        return 16.21;
+        return 16.22;
     }
 
     @Override
