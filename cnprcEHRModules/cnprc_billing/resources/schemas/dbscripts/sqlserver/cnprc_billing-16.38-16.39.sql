d62d773a68dec1945483301b9107953f40f626a5
Binal Patel <binalp@labkey.com>
2016-11-19 00:28:52 +0000

    Moved project_charge table and ETL items to cnprc_billing module; and modified Payor Assignments view accordingly. Also added files and config for incremental.
    SVN r47761 |2016-11-19 00:28:52 +0000

---
+++ cnprcEHRModules/cnprc_billing/resources/schemas/dbscripts/sqlserver/cnprc_billing-16.38-16.39.sql
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2016 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+CREATE TABLE cnprc_billing.project_charge (
+
+  payor_id nvarchar(10) NOT NULL,
+  pr_code nvarchar(5) NOT NULL,
+  charge_id nvarchar(4) NOT NULL,
+  fund_type nvarchar(1),
+  file_status nvarchar(2) NOT NULL,
+  objectid nvarchar(100),
+  Created DATETIME,
+  CreatedBy USERID,
+  Modified DATETIME,
+  ModifiedBy USERID,
+  Container	entityId NOT NULL,
+
+  CONSTRAINT PK_CNPRC_BILLING_PROJECT_CHARGE PRIMARY KEY (payor_id),
+  CONSTRAINT FK_CNPRC_BILLING_PROJECT_CHARGE_CONTAINER FOREIGN KEY (Container) REFERENCES core.Containers (EntityId)
+);
+GO
+
+CREATE INDEX CNPRC_BILLING_PROJECT_CHARGE_CONTAINER_INDEX ON cnprc_billing.project_charge (Container);
+GO
