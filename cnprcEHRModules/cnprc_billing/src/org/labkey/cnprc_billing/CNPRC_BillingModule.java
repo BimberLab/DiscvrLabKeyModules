3518195f3e775bbeaead8bd66758669ca3f20497
Binal Patel <binalp@labkey.com>
2016-12-13 07:41:00 +0000

    Added objectid index to cnprc_billing tables.
    SVN r48125 |2016-12-13 07:41:00 +0000

--- cnprcEHRModules/cnprc_billing/src/org/labkey/cnprc_billing/CNPRC_BillingModule.java
+++ cnprcEHRModules/cnprc_billing/src/org/labkey/cnprc_billing/CNPRC_BillingModule.java
@@ -40,7 +40,7 @@ public class CNPRC_BillingModule extends ExtendedSimpleModule
     @Override
     public double getVersion()
     {
-        return 16.391;
+        return 16.392;
     }
 
     @Override
