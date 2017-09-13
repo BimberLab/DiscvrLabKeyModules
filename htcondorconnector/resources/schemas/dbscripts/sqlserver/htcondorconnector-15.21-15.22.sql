7a178ceeb1c55683ce88587ddd85f3b0117e8675
Adam Rauch <adam@labkey.com>
2017-09-13 05:39:42 +0000

    Unintentional commit while attempting to resolve a tree conflict... revert
    SVN r53701 |2017-09-13 05:39:42 +0000

--- htcondorconnector/resources/schemas/dbscripts/sqlserver/htcondorconnector-15.21-15.22.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE htcondorconnector.condorJobs DROP COLUMN nodeId;
-ALTER TABLE htcondorconnector.condorJobs DROP COLUMN isActive;
-ALTER TABLE htcondorconnector.condorJobs DROP COLUMN hadError;
-
-ALTER TABLE htcondorconnector.condorJobs ADD hasStarted bit;
-ALTER TABLE htcondorconnector.condorJobs ADD status NVARCHAR(100);
-ALTER TABLE htcondorconnector.condorJobs ADD location NVARCHAR(1000);
-ALTER TABLE htcondorconnector.condorJobs ADD activeTaskId NVARCHAR(1000);
