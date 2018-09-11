bf5b99a18b810e6d56887cc7dead9b8f5fd5eb8e
David Smith <DSmith@txbiomed.org>
2018-09-11 17:01:46 +0000

    added snprc_scheduler module
    SVN r60128 |2018-09-11 17:01:46 +0000

---
+++ snprcEHRModules/snprc_scheduler/src/org/labkey/snprc_scheduler/Snprc_schedulerController.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2018 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.labkey.snprc_scheduler;
+
+import org.labkey.api.action.SimpleViewAction;
+import org.labkey.api.action.SpringActionController;
+import org.labkey.api.security.RequiresPermission;
+import org.labkey.api.security.permissions.ReadPermission;
+import org.labkey.api.view.JspView;
+import org.labkey.api.view.NavTree;
+import org.springframework.validation.BindException;
+import org.springframework.web.servlet.ModelAndView;
+
+public class Snprc_schedulerController extends SpringActionController
+{
+    private static final DefaultActionResolver _actionResolver = new DefaultActionResolver(Snprc_schedulerController.class);
+    public static final String NAME = "snprc_scheduler";
+
+    public Snprc_schedulerController()
+    {
+        setActionResolver(_actionResolver);
+    }
+
+    @RequiresPermission(ReadPermission.class)
+    public class BeginAction extends SimpleViewAction
+    {
+        public ModelAndView getView(Object o, BindException errors)
+        {
+            return new JspView("/org/labkey/snprc_scheduler/view/hello.jsp");
+        }
+
+        public NavTree appendNavTrail(NavTree root)
+        {
+            return root;
+        }
+    }
+}
