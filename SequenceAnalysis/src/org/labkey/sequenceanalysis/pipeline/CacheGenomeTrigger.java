f74412b9314a41d30b73833d381f73fa6293a1d4
bbimber <bbimber@gmail.com>
2020-10-19 13:42:56 -0700

    Update log4j version

--- SequenceAnalysis/src/org/labkey/sequenceanalysis/pipeline/CacheGenomeTrigger.java
+++ SequenceAnalysis/src/org/labkey/sequenceanalysis/pipeline/CacheGenomeTrigger.java
@@ -1,6 +1,6 @@
 package org.labkey.sequenceanalysis.pipeline;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.Logger;
 import org.labkey.api.data.Container;
 import org.labkey.api.module.ModuleLoader;
 import org.labkey.api.pipeline.PipeRoot;
