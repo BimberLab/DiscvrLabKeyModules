41aa69a11fee6cd220bac3e59d5a518261083e3e
bbimber <bbimber@gmail.com>
2020-10-28 14:16:45 -0700

    Update log4j

--- SequenceAnalysis/api-src/org/labkey/api/sequenceanalysis/pipeline/ReferenceGenomeManager.java
+++ SequenceAnalysis/api-src/org/labkey/api/sequenceanalysis/pipeline/ReferenceGenomeManager.java
@@ -3,7 +3,6 @@ package org.labkey.api.sequenceanalysis.pipeline;
 import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.Logger;
 import com.google.common.io.Files;
-import org.apache.log4j.Logger;
 import org.labkey.api.pipeline.PipelineJobException;
 import org.labkey.api.sequenceanalysis.run.SimpleScriptWrapper;
 
