580698e469c9ba32e3f4e1872ce862e7ac00cda0
bbimber <bbimber@gmail.com>
2020-10-30 10:46:50 -0700

    adjust rsync args

--- SequenceAnalysis/api-src/org/labkey/api/sequenceanalysis/pipeline/ReferenceGenomeManager.java
+++ SequenceAnalysis/api-src/org/labkey/api/sequenceanalysis/pipeline/ReferenceGenomeManager.java
@@ -93,7 +93,7 @@ public class ReferenceGenomeManager
 
         //Note: neither source nor dest have trailing slashes, so the entire source (i.e '128', gets synced into a subdir of dest)
         new SimpleScriptWrapper(log).execute(Arrays.asList(
-                "rsync", "-r", "-a", "--delete", "--delete-excluded", "--no-owner", "--no-group", sourceDir.getPath(), localCacheDir.getPath()
+                "rsync", "-r", "-a", "--delete", "--no-owner", "--no-group", sourceDir.getPath(), localCacheDir.getPath()
         ));
 
         try
