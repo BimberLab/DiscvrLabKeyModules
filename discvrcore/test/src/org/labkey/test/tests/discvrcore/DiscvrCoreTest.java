375e9c3a5cb0b6adb376692bf304530056d94e27
bbimber <bbimber@gmail.com>
2020-12-28 11:56:57 -0800

    Fix build

--- discvrcore/test/src/org/labkey/test/tests/discvrcore/DiscvrCoreTest.java
+++ discvrcore/test/src/org/labkey/test/tests/discvrcore/DiscvrCoreTest.java
@@ -23,7 +23,6 @@ import org.junit.experimental.categories.Category;
 import org.labkey.test.BaseWebDriverTest;
 import org.labkey.test.TestTimeoutException;
 import org.labkey.test.categories.InDevelopment;
-import org.labkey.test.pages.discvrcore.BeginPage;
 
 import java.util.Collections;
 import java.util.List;
@@ -62,10 +61,6 @@ public class DiscvrCoreTest extends BaseWebDriverTest
     public void testDiscvrCoreModule()
     {
         _containerHelper.enableModule("DiscvrCore");
-        BeginPage beginPage = BeginPage.beginAt(this, getProjectName());
-        assertEquals(200, getResponseCode());
-        final String expectedHello = "Hello, and welcome to the DiscvrCore module.";
-        assertEquals("Wrong hello message", expectedHello, beginPage.getHelloMessage());
     }
 
     @Override
