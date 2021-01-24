7b9a7cc26184190d1ad496efd2acc6078e035b17
bbimber <bbimber@gmail.com>
2021-01-23 16:28:43 -0800

    Bugfix PrepareRawCounts

--- singlecell/resources/chunks/PrepareRawCounts.R
+++ singlecell/resources/chunks/PrepareRawCounts.R
@@ -2,7 +2,7 @@ for (datasetId in names(seuratObjects)) {
     seuratObj <- seuratObjects[[datasetId]]
 
     datasetName <- datasetIdToName[[datasetId]]
-    seuratObjs[[datasetId]] <- CellMembrane::ReadAndFilter10xData(dataDir = data[[datasetId]], datasetName = datasetName)
+    seuratObjs[[datasetId]] <- CellMembrane::ReadAndFilter10xData(dataDir = seuratObjects[[datasetId]], datasetName = datasetName)
 
     newSeuratObjects[[datasetId]] <- seuratObj
 }
