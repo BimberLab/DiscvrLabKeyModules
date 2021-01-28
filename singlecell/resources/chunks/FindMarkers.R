f5ab22f5abeaafb36563d4de96c4b607ec894791
bbimber <bbimber@gmail.com>
2021-01-28 09:04:31 -0800

    R bugfix

--- singlecell/resources/chunks/FindMarkers.R
+++ singlecell/resources/chunks/FindMarkers.R
@@ -1,7 +1,7 @@
 for (datasetId in names(seuratObjects)) {
     seuratObj <- seuratObjects[[datasetId]]
 
-    dt <- CellMembrane::Find_Markers(seuratObj, identFields = identFields, outFile = paste0(datasetId, '.', fieldName, '.markers.txt'))
+    dt <- CellMembrane::Find_Markers(seuratObj, identFields = identFields, outFile = paste0(datasetId, '.markers.txt'))
     print(dt)
 
     # Cleanup
