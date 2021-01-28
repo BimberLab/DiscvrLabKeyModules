2d9623043b8a5b724f57129310fc67271bb55eb6
bbimber <bbimber@gmail.com>
2021-01-28 06:19:05 -0800

    Add preemptive cleanup

--- singlecell/resources/chunks/NormalizeAndScale.R
+++ singlecell/resources/chunks/NormalizeAndScale.R
@@ -1,5 +1,8 @@
 for (datasetId in names(seuratObjects)) {
+    # Preemptively cleanup:
     seuratObj <- seuratObjects[[datasetId]]
+    seuratObjects[[datasetId]] <- NULL
+    gc()
 
     seuratObj <- CellMembrane::NormalizeAndScale(seuratObj, variableFeatureSelectionMethod = variableFeatureSelectionMethod)
 
