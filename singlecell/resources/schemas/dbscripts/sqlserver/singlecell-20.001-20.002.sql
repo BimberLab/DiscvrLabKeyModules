cd7fdd4d3d98977de2b54d2130ccd2ba767986ef
bbimber <bbimber@gmail.com>
2021-02-01 11:19:53 -0800

    Add sql semicolons

--- singlecell/resources/schemas/dbscripts/sqlserver/singlecell-20.001-20.002.sql
+++ singlecell/resources/schemas/dbscripts/sqlserver/singlecell-20.001-20.002.sql
@@ -1,3 +1,3 @@
 ALTER TABLE singlecell.citeseq_antibodies ADD barcodePattern VARCHAR(100);
 GO
-UPDATE singlecell.citeseq_antibodies SET barcodePattern = '5PNNNNNNNNNN(BC)' WHERE barcodeName LIKE '%TotalSeq-C%'
+UPDATE singlecell.citeseq_antibodies SET barcodePattern = '5PNNNNNNNNNN(BC)' WHERE barcodeName LIKE '%TotalSeq-C%';
