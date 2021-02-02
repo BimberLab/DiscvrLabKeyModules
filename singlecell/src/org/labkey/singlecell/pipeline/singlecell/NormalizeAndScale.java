67cf75515e4e1172398e0123fa3abe2ba1a11dcb
bbimber <bbimber@gmail.com>
2021-02-02 15:20:11 -0800

    Update field size

--- singlecell/src/org/labkey/singlecell/pipeline/singlecell/NormalizeAndScale.java
+++ singlecell/src/org/labkey/singlecell/pipeline/singlecell/NormalizeAndScale.java
@@ -26,10 +26,8 @@ public class NormalizeAndScale extends AbstractCellMembraneStep
                         put("allowBlank", false);
                     }}, "vst"),
                     SeuratToolParameter.create("blockSize", "Block Size", "This will be passed to block.size in Seurat::ScaleData, which determines the number of features processed as a time. Increasing might increase speed at a memory cost, and descreasing on large datasets might reduce memory at a cost of overall speed.", "ldk-integerfield", new JSONObject(){{
-                        put("allowBlank", false);
-                        put("height", 150);
-                        put("delimiter", ",");
-                    }}, 1000, "block.size", true)
+
+                    }}, null, "block.size", false)
             ), Arrays.asList("/sequenceanalysis/field/TrimmingTextArea.js"), null);
         }
 
--- singlecell/src/org/labkey/singlecell/pipeline/singlecell/RemoveCellCycle.java
+++ singlecell/src/org/labkey/singlecell/pipeline/singlecell/RemoveCellCycle.java
@@ -21,10 +21,8 @@ public class RemoveCellCycle extends AbstractCellMembraneStep
         {
             super("RemoveCellCycle", "Remove Cell Cycle", "OOSAP", "This will score cells by cell cycle phase and regress out cell cycle.", Arrays.asList(
                     SeuratToolParameter.create("blockSize", "Block Size", "This will be passed to block.size in Seurat::ScaleData, which determines the number of features processed as a time. Increasing might increase speed at a memory cost, and descreasing on large datasets might reduce memory at a cost of overall speed.", "ldk-integerfield", new JSONObject(){{
-                        put("allowBlank", false);
-                        put("height", 150);
-                        put("delimiter", ",");
-                    }}, 1000, "block.size", true)
+
+                    }}, null, "block.size", false)
             ), null, null);
         }
 
