c61cbcfea8c4febe0d2b34478a3679e8ba111065
bbimber <bbimber@gmail.com>
2021-02-02 11:31:53 -0800

    Fix sql error

--- singlecell/resources/schemas/dbscripts/postgresql/singlecell-20.001-20.002.sql
+++ singlecell/resources/schemas/dbscripts/postgresql/singlecell-20.001-20.002.sql
@@ -1,3 +1,3 @@
-ALTER TABLE singlecell.citeseq_antibodies ADD pattern VARCHAR(100);
+ALTER TABLE singlecell.citeseq_antibodies ADD barcodepattern VARCHAR(100);
 
 UPDATE singlecell.citeseq_antibodies SET barcodePattern = '5PNNNNNNNNNN(BC)' WHERE barcodeName LIKE '%TotalSeq-C%';
