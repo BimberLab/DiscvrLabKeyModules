971505de551921a3bc8ec607b0e48df24eeac124
bbimber <bbimber@gmail.com>
2021-02-02 19:36:36 -0800

    Add better label

--- singlecell/src/org/labkey/singlecell/pipeline/singlecell/Downsample.java
+++ singlecell/src/org/labkey/singlecell/pipeline/singlecell/Downsample.java
@@ -23,7 +23,7 @@ public class Downsample extends AbstractCellMembraneStep
                     SeuratToolParameter.create("targetCells", "Target Cells Per Unit of Data", "Each unit of data will be downsampled to this level", "ldk-integerfield", new JSONObject(){{
                         put("allowBlank", false);
                     }}, null),
-                    SeuratToolParameter.create("subsetFields", "Additional Fields", "A comma-separated list of fields that will be used to subset data within each seurat object. For example, if 'barcodePrefix' is provided and 500 target cells is selected, each incoming seurat object will be subset to no more than 500 cells per barcodePrefix. If blank, each object will be treated as one unit of data.", "textfield", new JSONObject(){{
+                    SeuratToolParameter.create("subsetFields", "Additional Grouping Fields", "A comma-separated list of fields that will be used to subset data within each seurat object. For example, if 'barcodePrefix' is provided and 500 target cells is selected, each incoming seurat object will be subset to no more than 500 cells per barcodePrefix. If blank, each object will be treated as one unit of data.", "textfield", new JSONObject(){{
                         put("allowBlank", false);
                     }}, null),
                     SeuratToolParameter.create("seed", "Random Seed", "This random seed, used for downsampling", "ldk-integerfield", new JSONObject(){{
