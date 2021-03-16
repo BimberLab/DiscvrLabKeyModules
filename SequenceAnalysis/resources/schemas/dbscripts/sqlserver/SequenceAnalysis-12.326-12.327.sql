6f6b9aa0d4a0d846e75953ff21ba941a947e5c1a
bbimber <bbimber@gmail.com>
2021-03-16 11:00:45 -0700

    Fix typo in sql script

--- SequenceAnalysis/resources/schemas/dbscripts/sqlserver/SequenceAnalysis-12.326-12.327.sql
+++ SequenceAnalysis/resources/schemas/dbscripts/sqlserver/SequenceAnalysis-12.326-12.327.sql
@@ -1,5 +1,5 @@
-ALTER TABLE sequenceanalysis.aa_snps_by_codon ALTER column ref_nt VARCHAR(4000);
-ALTER TABLE sequenceanalysis.aa_snps_by_codon ALTER column q_nt VARCHAR(4000);
+ALTER TABLE sequenceanalysis.aa_snps_by_codon ALTER column ref_aa VARCHAR(4000);
+ALTER TABLE sequenceanalysis.aa_snps_by_codon ALTER column q_aa VARCHAR(4000);
 
 ALTER TABLE sequenceanalysis.nt_snps_by_pos ALTER column ref_nt VARCHAR(4000);
 ALTER TABLE sequenceanalysis.nt_snps_by_pos ALTER column q_nt VARCHAR(4000);
