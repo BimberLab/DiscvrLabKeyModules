7756868ae981913fd081fd4f09453bb455812398
jctrotter <trotter.james.jct@gmail.com>
2021-05-10 11:10:48 -0700

    import labkey/dev.config in jbrowse/dev.config

--- jbrowse/dev.config.js
+++ jbrowse/dev.config.js
@@ -4,51 +4,29 @@
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
  // COPY THIS FILE TO node_modules/@labkey/build/webpack/dev.config.js
-const lkModule = process.env.LK_MODULE;
+
+const devConfig = require('./node_modules/@labkey/build/webpack/dev.config')
+
 const entryPoints = require('./src/client/entryPoints');
 const constants = require('./node_modules/@labkey/build/webpack/constants');
 const webpack = require('webpack');
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 
-// set based on the lk module calling this config
-__dirname = lkModule;
+const clientConfig = devConfig
 
-const clientConfig = {
-
-    context: constants.context(__dirname),
-
-    mode: 'development',
-
-    devtool: 'eval',
-
-    entry: constants.processEntries(entryPoints),
-
-    output: {
-        path: constants.outputPath(__dirname),
-        publicPath: './', // allows context path to resolve in both js/css
-        filename: '[name].js'
-    },
-
-    module: {
-        rules: constants.loaders.TYPESCRIPT.concat(constants.loaders.STYLE).concat(constants.loaders.FILES),
-    },
-
-    resolve: {
-        alias: constants.aliases.LABKEY_PACKAGES,
-        extensions: constants.extensions.TYPESCRIPT,
-        fallback: {
-            "buffer": require.resolve("buffer"),
-            "path": require.resolve("path-browserify"),
-            "stream": require.resolve("stream-browserify"),
-            "zlib": require.resolve("browserify-zlib"),
-            "vm": require.resolve("vm-browserify"),
-            "fs": false
-        }
-    },
-
-      plugins: [new webpack.ProvidePlugin({
-            regeneratorRuntime: 'regenerator-runtime'
-        }), new NodePolyfillPlugin()].concat(constants.processPlugins(entryPoints)),
+clientConfig.resolve.fallback =
+{
+    "buffer": require.resolve("buffer"),
+    "path": require.resolve("path-browserify"),
+    "stream": require.resolve("stream-browserify"),
+    "zlib": require.resolve("browserify-zlib"),
+    "vm": require.resolve("vm-browserify"),
+    "fs": false
 }
+
+clientConfig.plugins =
+    [new webpack.ProvidePlugin({regeneratorRuntime: 'regenerator-runtime'}),
+     new NodePolyfillPlugin()].concat(constants.processPlugins(entryPoints))
+
 
 module.exports = [clientConfig]
