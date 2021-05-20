d211d192de77112f524e7bd6e6a6fc4369ecea47
jctrotter <trotter.james.jct@gmail.com>
2021-05-20 12:51:55 -0700

    refactored and updated configs

--- jbrowse/config/dev.config.js
+++ jbrowse/config/dev.config.js
@@ -4,14 +4,14 @@
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
 
-const watchConfig = require('./node_modules/@labkey/build/webpack/watch.config')
+const devConfig = require('../node_modules/@labkey/build/webpack/dev.config')
 
-const entryPoints = require('./src/client/entryPoints');
-const constants = require('./node_modules/@labkey/build/webpack/constants');
+const entryPoints = require('../src/client/entryPoints');
+const constants = require('../node_modules/@labkey/build/webpack/constants');
 const webpack = require('webpack');
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 
-const clientConfig = watchConfig
+const clientConfig = devConfig
 
 clientConfig.resolve.fallback =
 {
--- jbrowse/config/watch.config.js
+++ jbrowse/config/watch.config.js
@@ -4,14 +4,14 @@
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
 
-const prodConfig = require('./node_modules/@labkey/build/webpack/prod.config')
+const watchConfig = require('../node_modules/@labkey/build/webpack/watch.config')
 
-const entryPoints = require('./src/client/entryPoints');
-const constants = require('./node_modules/@labkey/build/webpack/constants');
+const entryPoints = require('../src/client/entryPoints');
+const constants = require('../node_modules/@labkey/build/webpack/constants');
 const webpack = require('webpack');
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 
-const clientConfig = prodConfig
+const clientConfig = watchConfig
 
 clientConfig.resolve.fallback =
 {
