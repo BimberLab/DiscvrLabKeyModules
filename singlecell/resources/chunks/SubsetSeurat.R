ecb43ec559a00179f803e5b8406a671f3b597726
bbimber <bbimber@gmail.com>
2021-05-22 05:10:14 -0700

    Restore bug workaround

--- singlecell/resources/chunks/SubsetSeurat.R
+++ singlecell/resources/chunks/SubsetSeurat.R
@@ -2,6 +2,11 @@ for (datasetId in names(seuratObjects)) {
     seuratObj <- seuratObjects[[datasetId]]
     seuratObjects[[datasetId]] <- NULL
 
+    #TODO: this is a stopgap for a former bug in RunCellHashing. Retain until all existing seurat objects lacking this field are removed
+    if (!'HTO.Classification' %in% names(seuratObj@meta.data) && 'consensuscall.global' %in% names(seuratObj@meta.data)) {
+        seuratObj$HTO.Classification <- seuratObj$consensuscall.global
+    }
+
     <SUBSETS>
 
     newSeuratObjects[[datasetId]] <- seuratObj
