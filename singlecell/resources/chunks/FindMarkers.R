27cdca0494f1f7be3675e7363eeb5fa38e5f32e2
bbimber <bbimber@gmail.com>
2021-05-26 08:24:42 -0700

    Bugfix output of FindMarkers

--- singlecell/resources/chunks/FindMarkers.R
+++ singlecell/resources/chunks/FindMarkers.R
@@ -1,12 +1,16 @@
+plotlist <- list()
 for (datasetId in names(seuratObjects)) {
     seuratObj <- seuratObjects[[datasetId]]
     seuratObjects[[datasetId]] <- NULL
 
-    outFile = paste0(datasetId, '.markers.txt')
+    outFile = paste0(outputPrefix, '.', datasetId, '.markers.txt')
     dt <- bindArgs(CellMembrane::Find_Markers, seuratObj)()
-    print(dt)
+    dt$x$caption <- paste0('<caption>Top DE Genes: ', datasetId, '</caption>')
+    plotList[[datasetId]] <- dt
 
     # Cleanup
     rm(seuratObj)
     gc()
 }
+
+htmltools::tagList(setNames(plotlist, NULL))
