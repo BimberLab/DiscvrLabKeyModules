14fbcc96fbb35aaa5a348f4b84c1983a2c65ead3
bbimber <bbimber@gmail.com>
2021-06-25 07:35:44 -0700

    Try to work around 'unable to delete file' problem with certain FF versions

--- SequenceAnalysis/test/src/org/labkey/test/tests/external/labModules/SequenceTest.java
+++ SequenceAnalysis/test/src/org/labkey/test/tests/external/labModules/SequenceTest.java
@@ -1500,7 +1500,8 @@ public class SequenceTest extends BaseWebDriverTest
         List<String> fileNames = Arrays.asList("fakeData.gff", "fakeData.bed");
         for (String fn : fileNames)
         {
-            File target = new File(fileRoot, fn);
+            //NOTE: this name change is to avoid an issue that seems to happen with certain FF versions, where the upload above leaves this file unable to get deleted
+            File target = new File(fileRoot, fn.replaceAll("fakeData", "fakeData2"));
             if (target.exists())
             {
                Assert.assertTrue("Unable to delete file: " + target.getPath(), target.delete());
