b156f4250d0e83bf4c38f3ca6b160894051bc44d
jctrotter <trotter.james.jct@gmail.com>
2021-08-09 17:07:20 -0700

    cleaned up code, improved readability

--- jbrowse/src/client/JBrowse/Browser/plugins/ExtendedVariantPlugin/ExtendedVariantWidget/ExtendedVariantWidget.js
+++ jbrowse/src/client/JBrowse/Browser/plugins/ExtendedVariantPlugin/ExtendedVariantWidget/ExtendedVariantWidget.js
@@ -19,12 +19,11 @@ export default jbrowse => {
     const { makeStyles } = jbrowse.jbrequire('@material-ui/core/styles')
     var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
     var _core = require("@material-ui/core");
-    var _styles = require("@material-ui/core/styles")
     var _SanitizedHTML = _interopRequireDefault(require("@jbrowse/core/ui/SanitizedHTML"));
     const queryParam = new URLSearchParams(window.location.search);
     const session = queryParam.get('session')
-
-
+    var fields = require("./fields").fields
+    var styles = require("./style").style
     const { observer, PropTypes: MobxPropTypes } = jbrowse.jbrequire('mobx-react')
     const PropTypes = jbrowse.jbrequire('prop-types')
     const React = jbrowse.jbrequire('react')
@@ -33,275 +32,7 @@ export default jbrowse => {
     const { FeatureDetails, BaseFeatureDetails, BaseCard } = jbrowse.jbrequire(
       '@jbrowse/core/BaseFeatureWidget/BaseFeatureDetail',
     )
-    const fields = {
-        ANN: {
-            title: 'Predicted Impact (SnpEff)'
-        },
-        CADD_PH: {
-            title: 'CADD Functional Prediction Score'
-        },
-        ENC: {
-            title: 'ENCODE Class'
-        },
-        ENN: {
-            title: 'ENCODE Feature Name'
-        },
-        ENCDNA_SC: {
-            title: 'ENCODE DNase Sensitivity Score'
-        },
-        ENCTFBS_SC: {
-            title: 'ENCODE Transcription Factor Binding Site Score'
-        },
-        ENCTFBS_TF: {
-            title: 'ENCODE Transcription Factors'
-        },
-        ENCSEG_NM: {
-            title: 'ENCODE Segmentation Status'
-        },
-        FS_EN: {
-            title: 'Funseq Annotated?'
-        },
-        FS_NS: {
-            title: 'Funseq Noncoding Score',
-            range: [0,5.4]
-        },
-        FS_SN: {
-            title: 'Funseq Sensitive Region?'
-        },
-        FS_TG: {
-            title: 'Funseq Target Gene?'
-        },
-        FS_US: {
-            title: 'Funseq Ultra-Sensitive Region?'
-        },
-        FS_WS: {
-            title: 'Funseq Score'
-        },
-        FS_SC: {
-            title: 'Funseq Non-coding Score',
-            range: [0,6]
-        },
-        SF: {
-            title: 'Swiss Prot Protein Function'
-        },
-        SX: {
-            title: 'Swiss Prot Expression'
-        },
-        SD: {
-            title: 'Swiss Prot Disease Annotations'
-        },
-        SM: {
-            title: 'Swiss Prot Post-translational Modifications'
-        },
-        RFG: {
-            title: 'Mutation Type (RefSeq)'
-        },
-        PC_PL: {
-            title: 'Conservation Score Among 46 Placental Mammals (PhastCons)'
-        },
-        PC_PR: {
-            title: 'Conservation Score Among 46 Primates (PhastCons)'
-        },
-        PC_VB: {
-            title: 'Conservation Score Among 100 Vertebrate Species (PhastCons)'
-        },
-        PP_PL: {
-            title: 'Conservation Score Among 46 Placental Mammals (Phylop)'
-        },
-        PP_PR: {
-            title: 'Conservation Score Among 46 Primates (Phylop)'
-        },
-        PP_VB: {
-            title: 'Conservation Score Among 100 Vertebrate Species (Phylop)'
-        },
-        RDB_WS: {
-            title: 'RegulomeDB Score',
-            range: [1,6]
-        },
-        RDB_MF: {
-            title: 'RegulomeDB Motifs'
-        },
-        MAF: {
-            title: 'Minor Allele Frequency'
-        },
-        OMIMN: {
-            title: 'OMIM Number'
-        },
-        OMIMM: {
-            title: 'OMIM Method'
-        },
-        OMIMC: {
-            title: 'OMIM Comments'
-        },
-        OMIMMUS: {
-            title: 'OMIM Mouse Correlate'
-        },
-        OMIMD: {
-            title: 'OMIM Disorders'
-        },
-        OMIMS: {
-            title: 'OMIM Status'
-        },
-        OMIMT: {
-            title: 'OMIM Title'
-        },
-        GRASP_PH: {
-            title: 'GRASP Phenotype'
-        },
-        GRASP_AN: {
-            title: 'GRASP Ancestry'
-        },
-        GRASP_P: {
-            title: 'GRASP p-value'
-        },
-        GRASP_PL: {
-            title: 'GRASP Platform'
-        },
-        GRASP_PMID: {
-            title: 'GRASP PMID'
-        },
-        GRASP_RS: {
-            title: 'GRASP SNP RS Number'
-        },
-        ERBCTA_NM: {
-            title: 'Ensembl Regulatory Build Predicted State'
-        },
-        LF: {
-            title: 'Unable to Lift to Human'
-        },
-        NE: {
-            title: 'PolyPhen2 Score'
-        },
-        NF: {
-            title: 'PolyPhen2 Prediction'
-        },
-        NG: {
-            title: 'nsdb LRT Score'
-        },
-        NH: {
-            title: 'nsdb LRT Prediction'
-        },
-        NK: {
-            title: 'MutationAssessor Score'
-        },
-        NL: {
-            title: 'MutationAssessor Prediction'
-        },
-        NC: {
-            title: 'nsdb SIFT Score'
-        },
-        NJ: {
-            title: 'MutationTaster Prediction'
-        },
-        LOF: {
-            title: 'Predicted Loss-of-function'
-        },
-        FC: {
-            title: 'Probable Promoter (FAMTOM5)'
-        },
-        FE: {
-            title: 'Probable Enhancer (FANTOM5)'
-        },
-        TMAF: {
-            title: '1000Genomes Allele Frequency'
-        },
-        CLN_SIG: {
-            title: 'ClinVar Significance'
-        },
-        CLN_DN: {
-            title: 'ClinVar Disease/Concept'
-        },
-        CLN_DNINCL: {
-            title: 'ClinVar Disease/Concept'
-        },
-        CLN_ALLELE: {
-            title: 'ClinVar Allele'
-        },
-        CLN_ALLELEID: {
-            title: 'ClinVar Allele ID'
-        },
-        CLN_VI: {
-            title: 'ClinVar Sources'
-        },
-        CLN_DBVARID: {
-            title: 'ClinVar dbVAR Accessions'
-        },
-        CLN_GENEINFO: {
-            title: 'ClinVar Gene(s)'
-        },
-        CLN_RS: {
-            title: 'ClinVar RS Numbers'
-        },
-        LiftedContig: {
-            title: 'Contig (Lifted to Human)'
-        },
-        LiftedStart: {
-            title: 'Start (Lifted to Human)'
-        }
-    }
-   var useStyles = (0, _styles.makeStyles)(function (theme) {
-      return {
-          table: {
-              padding: 0,
-              display: 'block'
-          },
-          link: {
-              color: 'rgb(0, 0, 238)',
-          },
-          message: {
-              paddingTop: theme.spacing(5),
-              paddingLeft: theme.spacing(5),
-              paddingRight: theme.spacing(5),
-              maxWidth: 500
-          },
-        expansionPanelDetails: {
-          display: 'block',
-          padding: theme.spacing(1)
-        },
-        expandIcon: {
-          color: '#FFFFFF'
-        },
-        paperRoot: {
-          background: theme.palette.grey[100]
-        },
-        field: {
-          display: 'flex',
-          flexWrap: 'wrap'
-        },
-        fieldDescription: {
-          '&:hover': {
-            background: 'yellow'
-          }
-        },
-        fieldName: {
-          wordBreak: 'break-all',
-          minWidth: '90px',
-          borderBottom: '1px solid #0003',
-          background: theme.palette.grey[200],
-          marginRight: theme.spacing(1),
-          padding: theme.spacing(0.5)
-        },
-        fieldValue: {
-          wordBreak: 'break-word',
-          maxHeight: 300,
-          padding: theme.spacing(0.5),
-          overflow: 'auto'
-        },
-        fieldSubValue: {
-          wordBreak: 'break-word',
-          maxHeight: 300,
-          padding: theme.spacing(0.5),
-          background: theme.palette.grey[100],
-          border: "1px solid ".concat(theme.palette.grey[300]),
-          boxSizing: 'border-box',
-          overflow: 'auto'
-        },
-        accordionBorder: {
-          marginTop: '4px',
-          border: '1px solid #444'
-        }
-      };
-    });
+    var useStyles = styles;
 
     function round(value, decimals) {
         return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
@@ -329,34 +60,26 @@ export default jbrowse => {
             }
         }
         return(
-        <BaseCard title="Predicted Function">
-            <Table className={classes.table}>
-                <TableHead>
-                    <TableRow className={classes.paperRoot}>
-                            <TableCell>Effect</TableCell>
-                            <TableCell>Impact</TableCell>
-                            <TableCell>Gene Name</TableCell>
-                            <TableCell>Position/Consequence</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {tableBodyRows}
-                </TableBody>
-            </Table>
-        </BaseCard>
+            <BaseCard title="Predicted Function">
+                <Table className={classes.table}>
+                    <TableHead>
+                        <TableRow className={classes.paperRoot}>
+                                <TableCell>Effect</TableCell>
+                                <TableCell>Impact</TableCell>
+                                <TableCell>Gene Name</TableCell>
+                                <TableCell>Position/Consequence</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {tableBodyRows}
+                    </TableBody>
+                </Table>
+            </BaseCard>
         )
-
-// A|intron_variant|MODIFIER|NTNG1|ENSMMUG00000008197|transcript|ENSMMUT00000072133.2|protein_coding|2/9|c.247-3863G>T||||||
-// 0|1             |2       |3    |4                 |5         |6                   |7             |8  |9            ||||||
-// Effect | Impact | Gene Name | Position / Consequence
-// downstream_gene_variant | MODIFIER | LTB-TNF(ENSMMUG...-ENSMMUT...) | c.247-3863G
-// 1 | 2 | 3+4 ask | 9+10
-
     }
 
     function makeDisplays(feat, displays, classes){
         var propertyJSX = []
-
         for(var display in displays){
             var tempProp = []
             for(var property in displays[display].properties){
@@ -403,13 +126,6 @@ export default jbrowse => {
                 propertyJSX.push(tempProp)
             }
         }
-                 /* <div style={{ padding: '7px', width: '100%', maxHeight: 600, overflow: 'auto' }}>
-                        <Table className={displays[i].name}>
-                            <TableHead>
-                                {propertyJSX[i]}
-                            </TableHead>
-                        </Table>
-                   </div>*/
         var displayJSX = []
         for(var i = 0; i < propertyJSX.length; i++){
             displayJSX.push(
@@ -590,12 +306,6 @@ export default jbrowse => {
         }
         var infoDisplays = makeDisplays(feat, infoConfig, classes)
         feat["INFO"] = null
-        /*for(var infoEntry in feat["INFO"]){
-            if(fields[infoEntry]){
-                feat[fields[infoEntry].title] = feat["INFO"][infoEntry]
-                feat["INFO"][infoEntry] = null
-            }
-        }*/
 
         return (
             <Paper className={classes.root} data-testid="extended-variant-widget">
