d2aa6b8cfe25418c24b1cb661e33d28bbfdb29e4
jctrotter <trotter.james.jct@gmail.com>
2021-09-10 09:59:44 -0700

    model.js code styling

--- jbrowse/src/client/JBrowse/Browser/plugins/ExtendedVariantPlugin/ExtendedVariantDisplay/model.js
+++ jbrowse/src/client/JBrowse/Browser/plugins/ExtendedVariantPlugin/ExtendedVariantDisplay/model.js
@@ -13,32 +13,32 @@ const attributes = ['SNV', 'Insertion', 'Deletion', 'High', 'Moderate', 'Low', '
 const colors = ['green', 'red', 'blue', 'gray', 'goldenrod']
 
 export default jbrowse => {
-  const configSchema = jbrowse.jbrequire(configSchemaF)
-  const { BaseLinearDisplay } = jbrowse.getPlugin(
-    'LinearGenomeViewPlugin',
-  ).exports
+   const configSchema = jbrowse.jbrequire(configSchemaF)
+   const { BaseLinearDisplay } = jbrowse.getPlugin(
+      'LinearGenomeViewPlugin',
+   ).exports
 
-    return types
-    .compose(
-      'ExtendedVariantDisplay',
-      BaseLinearDisplay,
-      types.model({
-        type: types.literal('ExtendedVariantDisplay'),
-        configuration: ConfigurationReference(configSchema),
-        colorSNV: types.maybe(types.string),
-        colorDeletion: types.maybe(types.string),
-        colorInsertion: types.maybe(types.string),
-        colorOther: types.maybe(types.string),
-        colorModerate: types.maybe(types.string),
-        colorHigh: types.maybe(types.string),
-        colorLow: types.maybe(types.string)
-      }),
-    )
-    .actions(self => ({
-       setReady(flag) {
-          self.ready = flag
-        },
-        setColor(attr, color){
+   return types
+      .compose(
+         'ExtendedVariantDisplay',
+         BaseLinearDisplay,
+         types.model({
+            type: types.literal('ExtendedVariantDisplay'),
+            configuration: ConfigurationReference(configSchema),
+            colorSNV: types.maybe(types.string),
+            colorDeletion: types.maybe(types.string),
+            colorInsertion: types.maybe(types.string),
+            colorOther: types.maybe(types.string),
+            colorModerate: types.maybe(types.string),
+            colorHigh: types.maybe(types.string),
+            colorLow: types.maybe(types.string)
+         }),
+      )
+      .actions(self => ({
+         setReady(flag){
+            self.ready = flag
+         },
+         setColor(attr, color) {
             if (attr == "SNV"){
                 self.colorSNV = color
             }
@@ -60,151 +60,140 @@ export default jbrowse => {
             else if (attr == "Low"){
                 self.colorLow = color
             }
-        },
-    }))
-    .actions(self => ({
-      afterAttach() {
-        addDisposer(
-          self,
-          autorun(
-            async () => {
-              try {
-                const { rpcManager } = getSession(self)
-                const colorSNV = self.colorSNV ?? 'blue'
-                const colorDeletion = self.colorDeletion ?? 'red'
-                const colorInsertion = self.colorInsertion ?? 'green'
-                const colorOther = self.colorOther ?? 'gray'
-                const colorHigh = self.colorHigh ?? 'red'
-                const colorModerate = self.colorModerate ?? 'goldenrod'
-                const colorLow = self.colorLow ?? 'black'
-                const color = "jexl:get(feature,'INFO').ANN['IMPACT']=='MODERATE'?'"+colorModerate+"':get(feature,'INFO').ANN['IMPACT']=='HIGH'?'"+colorHigh+"':get(feature,'INFO').ANN['IMPACT']=='LOW'?'"+colorLow+"':get(feature,'type')=='SNV'?'"+colorSNV+"':get(feature,'type')=='deletion'?'"+colorDeletion+"':get(feature,'type')=='insertion'?'"+colorInsertion+"':'"+colorOther+"'"
+         },
+      }))
+      .actions(self => ({
+         afterAttach() {
+            addDisposer(
+               self,
+               autorun(
+                  async () => {
+                     try {
+                        const { rpcManager } = getSession(self)
+                        const colorSNV = self.colorSNV ?? 'blue'
+                        const colorDeletion = self.colorDeletion ?? 'red'
+                        const colorInsertion = self.colorInsertion ?? 'green'
+                        const colorOther = self.colorOther ?? 'gray'
+                        const colorHigh = self.colorHigh ?? 'red'
+                        const colorModerate = self.colorModerate ?? 'goldenrod'
+                        const colorLow = self.colorLow ?? 'black'
+                        const color = "jexl:get(feature,'INFO').ANN['IMPACT']=='MODERATE'?'"+colorModerate+"':get(feature,'INFO').ANN['IMPACT']=='HIGH'?'"+colorHigh+"':get(feature,'INFO').ANN['IMPACT']=='LOW'?'"+colorLow+"':get(feature,'type')=='SNV'?'"+colorSNV+"':get(feature,'type')=='deletion'?'"+colorDeletion+"':get(feature,'type')=='insertion'?'"+colorInsertion+"':'"+colorOther+"'"
 
-                if(self.renderProps().config.color1.value != color){
-                  self.renderProps().config.color1.set(color)
-                  const view = getContainingView(self)
+                        if (self.renderProps().config.color1.value != color){
+                           self.renderProps().config.color1.set(color)
+                           const view = getContainingView(self)
 
-                  const { centerLineInfo } = getContainingView(self)
-                  const { refName, assemblyName, offset } = centerLineInfo
-                  const centerBp = Math.round(offset) + 1
+                           const { centerLineInfo } = getContainingView(self)
+                           const { refName, assemblyName, offset } = centerLineInfo
+                           const centerBp = Math.round(offset) + 1
 
-                  const region = {
-                    start: centerBp,
-                    end: (centerBp || 0) + 1,
-                    refName,
-                    assemblyName,
-                  }
+                           const region = {
+                              start: centerBp,
+                              end: (centerBp || 0) + 1,
+                              refName,
+                              assemblyName,
+                           }
 
-                  await (self.rendererType).renderInClient(rpcManager, {
-                    assemblyName,
-                    regions: [region],
-                    adapterConfig: self.adapterConfig,
-                    rendererType: self.rendererType.name,
-                    sessionId: getRpcSessionId(self),
-                    timeout: 1000000,
-                    ...self.renderProps(),
-                  })
-                  self.setReady(true)
-              } else {
-                  self.setReady(true)
-                }
-              } catch (error) {
-                console.error(error)
-                self.setError(error)
-              }
-            },
-            { delay: 1000 },
-          ),
-        )
-      },
-      selectFeature(feature) {
+                           await (self.rendererType).renderInClient(rpcManager, {
+                              assemblyName,
+                              regions: [region],
+                              adapterConfig: self.adapterConfig,
+                              rendererType: self.rendererType.name,
+                              sessionId: getRpcSessionId(self),
+                              timeout: 1000000,
+                              ...self.renderProps(),
+                           })
+                           self.setReady(true)
+                        } else {
+                           self.setReady(true)
+                        }
+                     } catch (error) {
+                        console.error(error)
+                        self.setError(error)
+                     }
+                  },
+                  { delay: 1000 },
+               ),
+            )
+         },
+         selectFeature(feature){
+            var extendedVariantDisplayConfig
+            if (getContainingTrack(self).configuration.metadata.value.extendedVariantDisplayConfig){
+               extendedVariantDisplayConfig = getContainingTrack(self).configuration.metadata.value.extendedVariantDisplayConfig
+            }
+            else {
+               extendedVariantDisplayConfig = []
+            }
 
-        var extendedVariantDisplayConfig
-        if(getContainingTrack(self).configuration.metadata.value.extendedVariantDisplayConfig){
-            extendedVariantDisplayConfig = getContainingTrack(self).configuration.metadata.value.extendedVariantDisplayConfig
-        }
-        else{
-            extendedVariantDisplayConfig = []
-        }
+            var message
+            if (getContainingTrack(self).configuration.metadata.value.message){
+               message = getContainingTrack(self).configuration.metadata.value.message
+            }
+            else {
+               message = ""
+            }
+            const trackId = getContainingTrack(self).configuration.trackId
+            const session = getSession(self)
+            var widgetId = 'Variant-' + trackId;
 
-        var message
-        if (getContainingTrack(self).configuration.metadata.value.message){
-            message = getContainingTrack(self).configuration.metadata.value.message
-        }
-        else{
-            message = ""
-        }
-        const trackId = getContainingTrack(self).configuration.trackId
-        const session = getSession(self)
-        var widgetId = 'Variant-' + trackId;
+            const featureWidget = session.addWidget(
+               'ExtendedVariantWidget',
+               widgetId,
+               { featureData: feature.toJSON(),
+                 extendedVariantDisplayConfig: extendedVariantDisplayConfig,
+                 message: message }
+            )
+            session.showWidget(featureWidget)
+            session.setSelection(feature)
+         },
+      }))
 
-        const featureWidget = session.addWidget(
-          'ExtendedVariantWidget',
-          widgetId,
-          { featureData: feature.toJSON(),
-            extendedVariantDisplayConfig: extendedVariantDisplayConfig,
-            message: message }
-        )
-        session.showWidget(featureWidget)
-        session.setSelection(feature)
-      },
-    }))
-
-    .views(self => {
-        const { renderProps: superRenderProps } = self
-        const { trackMenuItems: superTrackMenuItems } = self
-        return {
+      .views(self => {
+         const { renderProps: superRenderProps } = self
+         const { trackMenuItems: superTrackMenuItems } = self
+         return {
             renderProps() {
-                return {
-                    ...superRenderProps(),
-                    config: self.configuration.renderer,
-                }
+               return {
+                  ...superRenderProps(),
+                  config: self.configuration.renderer,
+               }
             },
 
             get rendererTypeName() {
-                return self.configuration.renderer.type
+               return self.configuration.renderer.type
             },
 
-        get composedTrackMenuItems() {
-          return [
-            {
-              label: 'Color',
-              icon: PaletteIcon,
-              subMenu: [
-                ...attributes.map(
-                  option => {
-                    return {
-                      label: option,
-                      subMenu: [
-                        ...colors.map(
-                            color => {
-                                return {
-                                    label: color,
-                                    onClick: () => {
-                                        self.setColor(option, color)
-                                        self.ready = false
-                                    }
-                                }
+            get composedTrackMenuItems() {
+               return [{
+                  label: 'Color',
+                  icon: PaletteIcon,
+                  subMenu: [...attributes.map(option => {
+                     return {
+                       label: option,
+                       subMenu: [...colors.map(color => {
+                          return {
+                            label: color,
+                            onClick: () => {
+                                self.setColor(option, color)
+                                self.ready = false
                             }
-                        )
-                      ]
-                    }
-                  },
-                ),
-              ],
-             },
-             ]},
-
-      trackMenuItems() {
-        return [
-          ...this.composedTrackMenuItems,
-          {
-            label: 'Get Session',
-            onClick: ()  => {
-              console.log(getSnapshot(getSession(self)))
+                          }
+                       })]
+                     }
+                  })]
+               }]
             },
-            icon: FilterListIcon,
-          },
-        ]
-      },
-    }})
+
+            trackMenuItems() {
+               return [
+                  ...this.composedTrackMenuItems,
+                  {  label: 'Get Session',
+                     onClick: ()  => {
+                       console.log(getSnapshot(getSession(self)))
+                     },
+                     icon: FilterListIcon, },
+               ]
+            },
+         }
+      })
 }
