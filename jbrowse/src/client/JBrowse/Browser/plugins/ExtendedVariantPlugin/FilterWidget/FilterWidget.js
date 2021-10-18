b0685b9ad69d6892c117e74968de3efb5b949d56
jctrotter <trotter.james.jct@gmail.com>
2021-10-18 14:36:13 -0700

    Cleaned up FilterWidget.js

--- jbrowse/src/client/JBrowse/Browser/plugins/ExtendedVariantPlugin/FilterWidget/FilterWidget.js
+++ jbrowse/src/client/JBrowse/Browser/plugins/ExtendedVariantPlugin/FilterWidget/FilterWidget.js
@@ -1,25 +1,9 @@
-import {ActionURL} from "@labkey/api";
 import {style as styles} from "./style";
 import {filterMap as filters} from "./filters"
-import {expandFilters, expandedFilterListToObj, expandedFilterObjToList} from "./filterUtil"
+
 import {
   MenuItem,
   Select,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  Button,
-  List,
-  ListItem,
-  DialogActions,
-  Checkbox,
-  FormGroup,
-  FormControlLabel,
-  Tooltip,
-  IconButton,
-  AddIcon,
-  FormLabel,
   FormControl,
 } from '@material-ui/core'
 import { getContainingTrack, getSession, getContainingView, getContainingDisplay } from '@jbrowse/core/util'
@@ -28,40 +12,8 @@ import Filter from './FilterComponent'
 
 
 export default jbrowse => {
-    const {
-        Paper,
-        Table,
-        TableBody,
-        TableCell,
-        TableHead,
-        TableRow
-    } = jbrowse.jbrequire('@material-ui/core')
     const { observer, PropTypes: MobxPropTypes } = jbrowse.jbrequire('mobx-react')
     const React = jbrowse.jbrequire('react')
-    const { useState, useEffect } = React
-    const { FeatureDetails, BaseCard } = jbrowse.jbrequire(
-            '@jbrowse/core/BaseFeatureWidget/BaseFeatureDetail',
-    )
-
-    function strToBool(str){
-        if(str === "true"){
-            return true
-        } else {
-            return false
-        }
-    }
-
-   /*const useStyles = makeStyles(theme => ({
-     root: {
-       padding: theme.spacing(1, 3, 1, 1),
-       background: theme.palette.background.default,
-       overflowX: 'hidden',
-     },
-     formControl: {
-       margin: theme.spacing(1),
-       minWidth: 150,
-     },
-   }))*/
 
    function FilterForm(props){
       const classes = styles()
@@ -69,8 +21,6 @@ export default jbrowse => {
       let track = model.track
 
       const configFilters = readConfObject(track, ['adapter', 'filters'])
-      const expandedFilterList = expandFilters(configFilters)
-      const expandedFilters = expandedFilterListToObj(expandedFilterList)
 
       const [newField, setNewField] = React.useState('')
 
@@ -87,10 +37,6 @@ export default jbrowse => {
               </MenuItem>
           )
 
-      // TODO - ERROR PREVENTION WHEN INVALID FILTER PASSED
-      /*const filterComponents = Object.entries(configFilters).map(([key, val]) =>
-         <Filter filterString={val} track={track} index={Number(key)}/>
-      )*/
       return(
       <>
          {Object.entries(configFilters).map(([key, val]) =>
@@ -116,12 +62,3 @@ export default jbrowse => {
 
     return observer(FilterForm)
 }
-
-         /*<Paper>
-            <form className={classes.filterGroup} onSubmit={handleSubmit}>
-               <FormGroup >
-                 {labels}
-               </FormGroup>
-               <input className={classes.button} type="submit" value="Apply" />
-            </form>
-         </Paper>*/
