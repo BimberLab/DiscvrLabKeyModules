e0192dae5bcba4dceeb3fa699d3c587fc4c2e52e
jctrotter <trotter.james.jct@gmail.com>
2021-10-25 14:31:01 -0700

    Tidied up filterUtil

--- jbrowse/src/client/JBrowse/Browser/plugins/ExtendedVariantPlugin/FilterWidget/filterUtil.js
+++ jbrowse/src/client/JBrowse/Browser/plugins/ExtendedVariantPlugin/FilterWidget/filterUtil.js
@@ -1,10 +1,16 @@
 import {filterMap as fields} from "./filters"
 
+// 'unexpanded' indicates the raw, unconverted format of a filter. Generally generated from the config file or filter UI. Contains a field, an operator and a value, delineated by ':'
+// 'expanded' indicates that the filter's field, operator and values have been combined into their full functioning expression. Contains a field and an an expression, delineated by ':'
+// unexpanded ex: "AF:lt:0.1"
+// expanded ex:   "AF:variant.INFO.AF[0] < 0.1"
+
 export const operators = {
     lt: "<",
     gt: ">",
     eq: "=="
 }
+
 export function unexpandedFilterStringToObj(filter){
 // filter: string in the format 'field:operator:value'
 // returns said string as an object
@@ -14,11 +20,6 @@ export function unexpandedFilterStringToObj(filter){
       operator: splitFilter[1],
       value: splitFilter[2]
    }
-}
-
-export function unexpandedFilterObjToString(filter){
-    const filterString = filter.field + ":" + filter.operator + ":" + filter.value
-    return filterString
 }
 
 export function expandedFilterStringToObj(filter){
@@ -31,54 +32,24 @@ export function expandedFilterStringToObj(filter){
     }
 }
 
-export function expandedFilterListToObj(filters){
-// filters: list of strings in the format 'field:expression'
-// returns list as objects
-    let obj = {}
-    for(let i = 0; i < filters.length; i++){
-        obj[i] = expandedFilterStringToObj(filters[i])
-    }
-    return obj
-}
-
-export function expandedFilterObjToList(filters){
-// filters: obj of expanded filters
-// returns as list of expanded filter strings in format 'label:expression'
-    let filterList = []
-    for (const filter in filters){
-        filterList.push(filters[filter].label+":"+filters[filter].expression)
-    }
-    return filterList
-
-}
-
 export function isFilterStringExpanded(filter){
 // filter: string
 // returns true if filter fits in expanded filter format
     try {
-        let temp = filter.split(":")
-        if(temp.length == 2 || temp[1].includes('variant.INFO.')){
-            return true
-        }
-        return false
+      let temp = filter.split(":")
+      if(temp.length == 2 || temp[1].includes('variant.INFO.')){
+         return true
+      }
+      return false
     } catch {
-        return false
+      return false
     }
 }
 
-export function isFilterFieldValid(filterField){
-   try{
-      if(fields[filterField]){
-         return true
-      }
-   } catch (e){
-      return false
-   }
-   return false
-}
-
 export function isUnexpandedFilterValid(filter){
-   try{
+// filter: string
+// returns true if filter fits in unexpanded filter format
+   try {
       const filterProps = filter.split(":")
       if (filterProps.length != 3){
          console.error("Invalid filter - " + filter + " does not have a length of 3")
@@ -113,14 +84,15 @@ export function isUnexpandedFilterValid(filter){
          }
       }
       return true
-
-   }catch (e){
+   } catch (e){
       console.error("Error validating filter " + filter + " - " + e)
       return false
    }
 }
 
 export function removeInvalidUnexpandedFilters(filters){
+// filters: array of strings
+// returns an array of strings filterList with invalid unexpanded strings removed
    let filterList = []
    if(!filters){
       return filterList
@@ -137,11 +109,11 @@ export function removeInvalidUnexpandedFilters(filters){
 }
 
 export function expandFilters(filters) {
-// filters: list of strings with properties "field:operator:value"
-// returns a list of strings "field:expression:"
-// 'expanded' indicates that the filter's field, operator and values have been combined into their full functioning expression.
-// unexpanded ex: "AF:lt:0.1"
-// expanded ex:   "AF:variant.INFO.AF[0] < 0.1"
+// filters: list of strings
+// returns a list of expanded strings.
+// if a string is already expanded, no processing needed.
+// if a string is unexpanded, it is expanded
+// if a string does not meet formatting requirements, it is removed from the list
 
     let filterList = []
     if(!filters){
@@ -150,26 +122,22 @@ export function expandFilters(filters) {
     for(const filter of filters){
         try {
             if(isFilterStringExpanded(filter)){
-                // if expanded, do not expand
+                // if expanded, we use it as-is
                 filterList.push(filter)
                 continue
             }
             if(!isUnexpandedFilterValid(filter)){
-               // if invalid filter, skip it
+               // if invalid filter, skip it entirely
                continue
             }
             const filterProps = filter.split(":")
             const field = filterProps[0]
             const rawOperator = filterProps[1]
             const value = filterProps[2]
-             if(!(field && rawOperator && value && isFilterFieldValid(field))){
-                // if any prop is null or field is not valid, do not make filter
-                continue
-             }
-             const fieldLocation = fields[field].baseLocation;
-             const operator = operators[rawOperator];
-             const jexlExpression = "variant.INFO." + fieldLocation + operator + value
-             const expandedFilter = field + ":" + jexlExpression
+            const fieldLocation = fields[field].baseLocation;
+            const operator = operators[rawOperator];
+            const jexlExpression = "variant.INFO." + fieldLocation + operator + value
+            const expandedFilter = field + ":" + jexlExpression
             filterList.push(expandedFilter)
         } catch (e){
             console.error("Error parsing filter, skipping. " + e)
