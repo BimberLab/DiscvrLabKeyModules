15026d943608954d193b97efb34c4b4550767fb3
jctrotter <trotter.james.jct@gmail.com>
2021-11-22 13:12:01 -0800

    Initial styling

---
+++ jbrowse/src/client/JBrowse/Browser/plugins/ExtendedVariantPlugin/SampleFilterWidget/style.js
@@ -0,0 +1,12 @@
+var _styles = require("@material-ui/core/styles")
+export const style = (0, _styles.makeStyles)(function (theme) {
+   return {
+      formControl: {
+         margin: theme.spacing(1),
+         minWidth: 100,
+      },
+      applyButton: {
+         maxWidth: 25
+      },
+   };
+});
