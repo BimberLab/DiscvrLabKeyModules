6d26140473283f86dc05f6f5c139ebcb4654946d
bbimber <bbimber@gmail.com>
2021-12-30 06:09:38 -0800

    Add hook to set futures::plan in single-cell docker jobs

--- singlecell/resources/chunks/Functions.R
+++ singlecell/resources/chunks/Functions.R
@@ -92,3 +92,8 @@ print('Updating future.globals.maxSize')
 options(future.globals.maxSize = Inf)
 
 options('Seurat.memsafe' = TRUE)
+
+if (!is.null(Sys.getenv('SEURAT_MAX_THREADS'))) {
+    print(paste0('Setting future::plan workers to: ', Sys.getenv('SEURAT_MAX_THREADS')))
+    future::plan(strategy='multiprocess', workers=Sys.getenv('SEURAT_MAX_THREADS'))
+}
