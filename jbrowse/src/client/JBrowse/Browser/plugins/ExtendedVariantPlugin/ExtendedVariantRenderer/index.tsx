2754cd59666d0b3ea5e58f2464b90a97c8e5f40d
bbimber <bbimber@gmail.com>
2022-01-09 08:22:31 -0800

    Update to JBrowse 1.5.5, migrate to newer SvgRenderer, and reduce a lot of copied code

--- jbrowse/src/client/JBrowse/Browser/plugins/ExtendedVariantPlugin/ExtendedVariantRenderer/index.tsx
+++ jbrowse/src/client/JBrowse/Browser/plugins/ExtendedVariantPlugin/ExtendedVariantRenderer/index.tsx
@@ -1,6 +1,6 @@
 import BoxRendererType from '@jbrowse/core/pluggableElementTypes/renderers/BoxRendererType';
 
-export { default as ReactComponent } from './components/SvgFeatureRendering'
+export { default as ReactComponent } from './components/ExtendedVariantRenderering'
 export { default as configSchema } from './configSchema'
 
 export class ExtendedVariantRenderer extends BoxRendererType {
