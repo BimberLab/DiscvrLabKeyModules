31d1dafe92e8988261ffe986be59e8fd3eaedab1
Trey Chadick <tchad@labkey.com>
2022-03-15 11:09:35 -0700

    Make 'SequenceAnalysis-12.328-12.329.sql' compatible with Postgres 10 (#129)
    The `INCLUDE` clause isn't supported by Postgres 10

--- SequenceAnalysis/resources/schemas/dbscripts/postgresql/SequenceAnalysis-12.328-12.329.sql
+++ SequenceAnalysis/resources/schemas/dbscripts/postgresql/SequenceAnalysis-12.328-12.329.sql
@@ -7,13 +7,11 @@ CREATE INDEX IDX_asj_status_container_alignment_id_ref_nt_id ON sequenceanalysis
 
 CREATE INDEX IDX_readData_readset ON sequenceanalysis.readData (
 	readset ASC
-)
-INCLUDE(fileid1, fileid2, runid);
+);
 
 CREATE INDEX IDX_quality_metrics_metricname_dataid_readset ON sequenceanalysis.quality_metrics (
 	metricName ASC,
 	dataId ASC,
 	readset ASC
-)
-INCLUDE(metricValue);
+);
 
