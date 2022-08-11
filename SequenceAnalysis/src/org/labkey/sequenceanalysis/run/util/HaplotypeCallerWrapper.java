0aa8d9fc79c98b564a05237a381c1408b6e86bf3
bbimber <bbimber@gmail.com>
2022-08-11 10:56:03 -0700

    Add additional single-cell filters and support ReblockGVCF

--- SequenceAnalysis/src/org/labkey/sequenceanalysis/run/util/HaplotypeCallerWrapper.java
+++ SequenceAnalysis/src/org/labkey/sequenceanalysis/run/util/HaplotypeCallerWrapper.java
@@ -1,11 +1,12 @@
 package org.labkey.sequenceanalysis.run.util;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
 import org.labkey.api.pipeline.PipelineJobException;
 import org.labkey.api.sequenceanalysis.run.AbstractGatk4Wrapper;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +21,11 @@ public class HaplotypeCallerWrapper extends AbstractGatk4Wrapper
     }
 
     public void execute(File inputBam, File referenceFasta, File outputFile, List<String> options) throws PipelineJobException
+    {
+        execute(inputBam, referenceFasta, outputFile, options, true);
+    }
+
+    public void execute(File inputBam, File referenceFasta, File outputFile, List<String> options, boolean reblockGVCF) throws PipelineJobException
     {
         getLogger().info("Running GATK 4 HaplotypeCaller for: " + inputBam.getName());
 
@@ -71,6 +77,45 @@ public class HaplotypeCallerWrapper extends AbstractGatk4Wrapper
         {
             getLogger().debug("\tdeleting temp BAM index: " + expectedIndex.getPath());
             expectedIndex.delete();
+        }
+
+        if (reblockGVCF)
+        {
+            getLogger().info("Running GATK 4 ReblockGVCF for: " + outputFile.getName());
+
+            File reblockOutput = new File(outputFile.getParentFile(), "tempReblock.g.vcf.gz");
+
+            List<String> reblockArgs = new ArrayList<>(getBaseArgs());
+            reblockArgs.add("ReblockGVCF");
+            reblockArgs.add("-R");
+            reblockArgs.add(referenceFasta.getPath());
+            reblockArgs.add("-V");
+            reblockArgs.add(outputFile.getPath());
+            reblockArgs.add("-O");
+            reblockArgs.add(reblockOutput.getPath());
+
+            execute(reblockArgs);
+            if (!reblockOutput.exists())
+            {
+                throw new PipelineJobException("Expected output not found: " + reblockOutput.getPath());
+            }
+
+            File outputFileIdx = new File(outputFile.getPath() + ".tbi");
+
+            getLogger().debug("Replacing original gVCF with reblocked file");
+            outputFileIdx.delete();
+            outputFile.delete();
+
+            File reblockOutputIdx = new File(reblockOutput.getPath() + ".tbi");
+            try
+            {
+                FileUtils.moveFile(reblockOutput, outputFile);
+                FileUtils.moveFile(reblockOutputIdx, outputFileIdx);
+            }
+            catch (IOException e)
+            {
+                throw new PipelineJobException(e);
+            }
         }
     }
 }
