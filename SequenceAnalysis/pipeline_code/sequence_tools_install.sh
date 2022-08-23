c74aa63376822a70adcdf8fd21d3fb5de7e91c5d
bbimber <bbimber@gmail.com>
2022-08-23 11:48:18 -0700

    Update picard version for tests

--- SequenceAnalysis/pipeline_code/sequence_tools_install.sh
+++ SequenceAnalysis/pipeline_code/sequence_tools_install.sh
@@ -854,7 +854,7 @@ then
     rm -Rf $LKTOOLS_DIR/htsjdk-*
     rm -Rf $LKTOOLS_DIR/libIntelDeflater.so
 
-    wget $WGET_OPTS https://github.com/broadinstitute/picard/releases/download/2.18.4/picard.jar
+    wget $WGET_OPTS https://github.com/broadinstitute/picard/releases/download/2.27.4/picard.jar
 
     cp -R ./picard.jar $LKTOOLS_DIR/
 else
