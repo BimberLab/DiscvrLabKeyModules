1cd0f71c7349938187dea63d99e90c81532030c3
bbimber <bbimber@gmail.com>
2022-10-25 09:21:40 -0700

    Add UCell debugging

--- singlecell/resources/chunks/CalculateUCellScores.R
+++ singlecell/resources/chunks/CalculateUCellScores.R
@@ -2,6 +2,7 @@ for (datasetId in names(seuratObjects)) {
     printName(datasetId)
     seuratObj <- readRDS(seuratObjects[[datasetId]])
 
+    message(paste0('Loading dataset ', datasetId, ', with total cells: ', ncol(seuratObj)))
     seuratObj <- RIRA::CalculateUCellScores(seuratObj)
 
     saveData(seuratObj, datasetId)
