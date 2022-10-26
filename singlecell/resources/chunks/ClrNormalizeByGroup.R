86e05193b3e595b833bd6dbcc9600403fe49ddbd
bbimber <bbimber@gmail.com>
2022-10-25 20:39:26 -0700

    Add missing arguments

--- singlecell/resources/chunks/ClrNormalizeByGroup.R
+++ singlecell/resources/chunks/ClrNormalizeByGroup.R
@@ -3,7 +3,7 @@ for (datasetId in names(seuratObjects)) {
     seuratObj <- readRDS(seuratObjects[[datasetId]])
     gc()
 
-    seuratObj <- CellMembrane::ClrNormalizeByGroup(seuratObj, groupingVar = groupingVar, assayName = assayName, targetAssayName = targetAssayName, margin = margin, minCellsPerGroup = minCellsPerGroup, calculatePerFeatureUCell = calculatePerFeatureUCell)
+    seuratObj <- CellMembrane::ClrNormalizeByGroup(seuratObj, groupingVar = groupingVar, assayName = assayName, targetAssayName = targetAssayName, margin = margin, minCellsPerGroup = minCellsPerGroup, calculatePerFeatureUCell = calculatePerFeatureUCell, featureWhitelist = featureWhitelist, featureExclusionList = featureExclusionList)
 
     saveData(seuratObj, datasetId)
 
