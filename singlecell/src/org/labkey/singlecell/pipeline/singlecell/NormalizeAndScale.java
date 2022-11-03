fe025f6b6c3809ad9ae5c6687a26083471846a9b
bbimber <bbimber@gmail.com>
2022-11-03 11:29:18 -0700

    Allow users to set scoreCellCycle with NormalizeAndScale

--- singlecell/src/org/labkey/singlecell/pipeline/singlecell/NormalizeAndScale.java
+++ singlecell/src/org/labkey/singlecell/pipeline/singlecell/NormalizeAndScale.java
@@ -49,6 +49,9 @@ public class NormalizeAndScale extends AbstractCellMembraneStep
                     SeuratToolParameter.create("nVariableFeatures", "# Variable Features", "Controls the number of variable features that will be used. This only applies to the standard NormalizeData/ScaleData pipeline, not SCTransform", "ldk-integerfield", new JSONObject(){{
                         put("minValue", 0);
                     }}, null),
+                    SeuratToolParameter.create("scoreCellCycle", "Score Cell Cycle?", "If checked, ScoreCellCycle will be run to compute Phase, which is stored in meta.data. If a field named Phase already exists, this will be skipped", "checkbox", new JSONObject(){{
+                        put("checked", true);
+                    }}, true),
                     getSeuratThreadsParam()
                     ), Arrays.asList("/sequenceanalysis/field/TrimmingTextArea.js"), null);
         }
