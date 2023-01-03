7dc14625ac372aefbe69b469fc36249ba2bd0e91
bbimber <bbimber@gmail.com>
2023-01-03 10:05:48 -0800

    for CITE-seq, switch to use proper assay key when plotting

--- singlecell/resources/chunks/CiteSeqPlots.R
+++ singlecell/resources/chunks/CiteSeqPlots.R
@@ -7,7 +7,7 @@ for (datasetId in names(seuratObjects)) {
     } else {
         for (feat in rownames(seuratObj[[assayName]])) {
             tryCatch({
-                CellMembrane::FeaturePlotAcrossReductions(seuratObj, features = paste0(tolower(assayName), '_', feat))
+                CellMembrane::FeaturePlotAcrossReductions(seuratObj, features = paste0(seuratObj@assays[[assayName]]@key, feat))
             }, error = function(e){
                 warning(conditionMessage(e))
                 traceback()
