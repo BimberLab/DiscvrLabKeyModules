d4fe3ad2f24d2bcd22f50dac874349d947c0c495
bbimber <bbimber@gmail.com>
2023-03-21 14:03:20 -0700

    Split RunScGate step to make less confusing

--- singlecell/resources/chunks/RunScGate.R
+++ singlecell/resources/chunks/RunScGate.R
@@ -2,11 +2,7 @@ for (datasetId in names(seuratObjects)) {
     printName(datasetId)
     seuratObj <- readRDS(seuratObjects[[datasetId]])
 
-    if (useRhesusDefaults) {
-        seuratObj <- RIRA::RunScGateWithRhesusModels(seuratObj, dropAmbiguousConsensusValues = dropAmbiguousConsensusValues, assay = assayName)
-    } else {
-        seuratObj <- RIRA::RunScGateForModels(seuratObj, modelNames = modelNames, labelRename = list(Tcell = 'T_NK', NK = 'T_NK'), dropAmbiguousConsensusValues = dropAmbiguousConsensusValues, consensusModels = consensusModels, assay = assayName)
-    }
+    seuratObj <- RIRA::RunScGateWithRhesusModels(seuratObj, dropAmbiguousConsensusValues = dropAmbiguousConsensusValues, assay = assayName)
 
     saveData(seuratObj, datasetId)
 }
