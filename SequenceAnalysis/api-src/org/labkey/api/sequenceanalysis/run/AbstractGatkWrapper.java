07103e338da14260d39e57ca738d7be93eec18b7
bbimber <bbimber@gmail.com>
2023-04-03 14:09:23 -0700

    Update several tools to newer versions that no longer require java8

--- SequenceAnalysis/api-src/org/labkey/api/sequenceanalysis/run/AbstractGatkWrapper.java
+++ SequenceAnalysis/api-src/org/labkey/api/sequenceanalysis/run/AbstractGatkWrapper.java
@@ -54,38 +54,6 @@ abstract public class AbstractGatkWrapper extends AbstractCommandWrapper
         _maxRamOverride = maxRamOverride;
     }
 
-    protected void addJavaHomeToEnvironment()
-    {
-        //since GATK requires java8, set JAVA_HOME to match this:
-        File java8 = new File(SequencePipelineService.get().getJava8FilePath()).getParentFile();
-        if (java8.getParentFile() == null)
-        {
-            getLogger().debug("unexpected path to java8, cannot determine JAVA_HOME: " + java8.getPath());
-            return;
-        }
-
-        String javaDir = java8.getParentFile().getPath();
-        getLogger().debug("setting JAVA_HOME to java8 location: " + javaDir);
-        addToEnvironment("JAVA_HOME", javaDir);
-    }
-    
-    protected File getQueueJAR()
-    {
-        String path = PipelineJobService.get().getConfigProperties().getSoftwarePackagePath("GATKPATH");
-        if (path != null)
-        {
-            return new File(path);
-        }
-
-        path = PipelineJobService.get().getConfigProperties().getSoftwarePackagePath(SequencePipelineService.SEQUENCE_TOOLS_PARAM);
-        if (path == null)
-        {
-            path = PipelineJobService.get().getAppProperties().getToolsDirectory();
-        }
-
-        return path == null ? new File("Queue.jar") : new File(path, "Queue.jar");
-    }
-
     public boolean jarExists()
     {
         return getJAR() == null || !getJAR().exists();
@@ -98,69 +66,6 @@ abstract public class AbstractGatkWrapper extends AbstractCommandWrapper
 
         getLogger().info("\tensure dictionary exists");
         new CreateSequenceDictionaryWrapper(getLogger()).execute(referenceFasta, false);
-    }
-
-    public String getVersionString() throws PipelineJobException
-    {
-        List<String> args = new ArrayList<>();
-        args.add(SequencePipelineService.get().getJava8FilePath());
-        args.addAll(SequencePipelineService.get().getJavaOpts(_maxRamOverride));
-        args.add("-jar");
-        args.add(getJAR().getPath());
-        args.add("--version");
-
-        return StringUtils.trimToNull(executeWithOutput(args));
-    }
-
-    public Integer getMinRamPerQueueJob()
-    {
-        return _minRamPerQueueJob;
-    }
-
-    public void setMinRamPerQueueJob(Integer minRamPerQueueJob)
-    {
-        _minRamPerQueueJob = minRamPerQueueJob;
-    }
-
-    protected Integer getScatterForQueueJob()
-    {
-        // NOTE: Queue will create n number of jobs, dividing memory evenly between them.  Because it is possible
-        // to submit a job w/ lower available RAM and comparably high CPUs, this could result in queue not having enough memory per job.
-        // therefore do a quick check and potentially scale down scatter
-        Integer maxThreads = SequencePipelineService.get().getMaxThreads(getLogger());
-        if (maxThreads != null)
-        {
-            if (_minRamPerQueueJob != null && _minRamPerQueueJob > 0)
-            {
-                String maxRamSetting = StringUtils.trimToNull(System.getenv("SEQUENCEANALYSIS_MAX_RAM"));
-                if (maxRamSetting != null)
-                {
-                    try
-                    {
-                        Integer maxRamAllowed = ConvertHelper.convert(maxRamSetting, Integer.class);
-                        if (maxRamAllowed != null)
-                        {
-                            int adjusted = Math.max(maxRamAllowed / _minRamPerQueueJob, 1);
-                            if (adjusted < maxThreads)
-                            {
-                                getLogger().debug("lowering max threads to match available RAM.  setting to: " + adjusted);
-                                maxThreads = adjusted;
-                            }
-                        }
-                    }
-                    catch (ConvergenceException e)
-                    {
-                        getLogger().warn("non-numeric value for SEQUENCEANALYSIS_MAX_RAM: [" + maxRamSetting + "]");
-                    }
-                }
-            }
-        }
-        else
-        {
-            maxThreads = 1;
-        }
-
-        return maxThreads;
     }
 
     protected List<String> getBaseArgs()
