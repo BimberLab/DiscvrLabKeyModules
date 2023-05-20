dc101d26d7b849c349cced67521953848db2ceef
bbimber <bbimber@gmail.com>
2023-05-20 11:23:00 -0700

    Add stub for studies module

--- singlecell/resources/chunks/ClrNormalizeByGroup.R
+++ singlecell/resources/chunks/ClrNormalizeByGroup.R
@@ -1,5 +1,10 @@
 for (datasetId in names(seuratObjects)) {
     printName(datasetId)
+    if (!('ADT' %in% names(seuratObj@assays))) {
+        print('ADT assay not present, skipping')
+        next
+    }
+
     seuratObj <- readRDS(seuratObjects[[datasetId]])
     seuratObj <- CellMembrane::ClrNormalizeByGroup(seuratObj, groupingVar = groupingVar, assayName = assayName, targetAssayName = targetAssayName, margin = margin, minCellsPerGroup = minCellsPerGroup, calculatePerFeatureUCell = calculatePerFeatureUCell, featureInclusionList = featureWhitelist, featureExclusionList = featureExclusionList, doAsinhTransform = doAsinhTransform)
 
