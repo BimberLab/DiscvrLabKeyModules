f336c68c55cb79707c6f2d93b093a661ac668381
bbimber <bbimber@gmail.com>
2023-05-25 16:08:22 -0700

    Bugfix to ClrNormalizeByGroup when ADT not present

--- singlecell/resources/chunks/ClrNormalizeByGroup.R
+++ singlecell/resources/chunks/ClrNormalizeByGroup.R
@@ -1,12 +1,12 @@
 for (datasetId in names(seuratObjects)) {
     printName(datasetId)
-    if (!('ADT' %in% names(seuratObj@assays))) {
-        print('ADT assay not present, skipping')
-        next
-    }
 
     seuratObj <- readRDS(seuratObjects[[datasetId]])
-    seuratObj <- CellMembrane::ClrNormalizeByGroup(seuratObj, groupingVar = groupingVar, assayName = assayName, targetAssayName = targetAssayName, margin = margin, minCellsPerGroup = minCellsPerGroup, calculatePerFeatureUCell = calculatePerFeatureUCell, featureInclusionList = featureWhitelist, featureExclusionList = featureExclusionList, doAsinhTransform = doAsinhTransform)
+    if (!('ADT' %in% names(seuratObj@assays))) {
+        print('ADT assay not present, skipping')
+    } else {
+        seuratObj <- CellMembrane::ClrNormalizeByGroup(seuratObj, groupingVar = groupingVar, assayName = assayName, targetAssayName = targetAssayName, margin = margin, minCellsPerGroup = minCellsPerGroup, calculatePerFeatureUCell = calculatePerFeatureUCell, featureInclusionList = featureWhitelist, featureExclusionList = featureExclusionList, doAsinhTransform = doAsinhTransform)
+    }
 
     saveData(seuratObj, datasetId)
 
