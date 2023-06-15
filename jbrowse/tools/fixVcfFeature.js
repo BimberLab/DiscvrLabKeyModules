adb1224f4f74f44a7a5a10c820dd9a38820be356
bbimber <bbimber@gmail.com>
2023-06-15 16:25:09 -0700

    Improvement of file edit

--- jbrowse/tools/fixVcfFeature.js
+++ jbrowse/tools/fixVcfFeature.js
@@ -5,13 +5,12 @@ function readWriteAsync() {
     fs.readFile(fn, 'utf-8', function(err, data){
         if (err) throw err;
 
-        console.log('edit started');
-
-        data = "// @ts-nocheck\n" + data
+        if (!data.includes("ts-nocheck")) {
+            data = "// @ts-nocheck\n" + data
+        }
 
         fs.writeFile(fn, data, 'utf-8', function (err) {
             if (err) throw err;
-            console.log('edit complete');
         });
     });
 }
