78116be70d6f8b1072d7ca4604546a79fdc2ccc0
bbimber <bbimber@gmail.com>
2023-06-15 16:22:25 -0700

    Fix related to last commit

--- jbrowse/tools/fixVcfFeature.js
+++ jbrowse/tools/fixVcfFeature.js
@@ -17,17 +17,3 @@ function readWriteAsync() {
 }
 
 readWriteAsync();
-
-// function readWriteSync() {
-//     var data = fs.readFileSync('filelist.txt', 'utf-8');
-//
-//     var newValue = data.replace(/^\./gim, 'myString');
-//
-//     fs.writeFileSync('filelistSync.txt', newValue, 'utf-8');
-//
-//     console.log('readFileSync complete');
-// }
-//
-//
-// readWriteSync();
-//
