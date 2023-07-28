4f63a39918b187c958b33ffdc9c45976959010aa
bbimber <bbimber@gmail.com>
2023-07-28 05:51:56 -0700

    Fixes to search component

--- jbrowse/src/client/JBrowse/Search/components/StandaloneSearchComponent.tsx
+++ jbrowse/src/client/JBrowse/Search/components/StandaloneSearchComponent.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import BaseResult from '@jbrowse/core/TextSearch/BaseResults';
+import React from 'react';
 import { RefNameAutocomplete } from '@jbrowse/plugin-linear-genome-view';
 import { fetchResults } from '@jbrowse/plugin-linear-genome-view/esm/LinearGenomeView/components/util';
+import { ParsedLocString, parseLocString } from '@jbrowse/core/util';
 
 export default function StandaloneSearchComponent(props: { session: any, selectedRegion: string, assemblyName: string, onSelect: (string) => void, forVariantTable?: boolean}) {
     const { session, selectedRegion, assemblyName, onSelect, forVariantTable } = props
@@ -17,6 +17,11 @@ export default function StandaloneSearchComponent(props: { session: any, selecte
         return fetchResults( { queryString, assembly, textSearchManager, rankSearchResults, searchScope })
     }
 
+    const componentAssemblyName = assemblyName
+    const isValidRefNameForAssembly = function(refName: string, assemblyName?: string) {
+        return assemblyManager.isValidRefName(refName, assemblyName ?? componentAssemblyName)
+    }
+
     return (
         <span style={forVariantTable ? {display: 'inline-block', marginRight: '14px'} : {}}>
         <RefNameAutocomplete
@@ -25,49 +30,73 @@ export default function StandaloneSearchComponent(props: { session: any, selecte
           fetchResults={doFetch}
           value={selectedRegion}
           onSelect={option => {
+              let parsedLocString: ParsedLocString
               if (option.getLocation()) {
-                  onSelect(option?.getLocation())
+                  parsedLocString = parseLocString(option.getLocation(), isValidRefNameForAssembly)
               }
-              else if (option.results) {
+              else if (option.results?.length) {
                   let contigs = []
                   let minVal = -1
                   let maxVal = -1
 
+                  console.log(option)
+
                   option.results.forEach(r => {
                       if (r.getLocation()) {
-                          const l = r.getLocation().split(":")
-                          if (l.length !== 2) {
-                              console.error("Invalid location: " + r.getLocation())
-                              return false
+                          const l = parseLocString(r.getLocation(), isValidRefNameForAssembly)
+                          if (!l) {
+                              alert('Invalid location: ' + option.label)
+                              console.error('Invalid location: ' + option.label)
+                              return
                           }
 
-                          const [ start, end ] = l[1].split("..")
-                          if (isNaN(Number(start)) || isNaN(Number(end))) {
-                              console.error("Invalid location: " + r.getLocation())
-                              return false
+                          // ParsedLocation.start is 0-based
+                          let start = l.start ?? -1
+                          let end = l.end ?? -1
+                          if (start === -1 || end === -1) {
+                            alert('Location lacks a start or end, cannot use: ' + r.label)
+                            return false
                           }
 
-                          contigs.push(l[0])
-                          if (minVal == -1 || minVal > Number(start)) {
-                              minVal = Number(start)
+                          contigs.push(l.refName)
+
+                          if (minVal === -1 || minVal > start) {
+                              minVal = start
                           }
 
-                          if (maxVal == -1 || maxVal < Number(end)) {
-                              maxVal = Number(end)
+                          if (maxVal === -1 || maxVal < end) {
+                              maxVal = end
                           }
                       }
                   })
 
                   if (contigs.length !== 1) {
-                      console.error("Invalid location after parse")
+                      alert('Invalid location: ' + selectedRegion)
+                      console.error("Invalid location after parse: " + selectedRegion)
                       return null
                   }
                   else {
-                      return(contigs[0] + ":" + minVal + "-" + maxVal)
+                      return(contigs[0] + ":" + (minVal + 1) + "-" + maxVal)
                   }
               }
+              else if (option.label) {
+                  parsedLocString = parseLocString(option.label, isValidRefNameForAssembly)
+
+              }
+
+              if (parsedLocString) {
+                  let start = parsedLocString.start ?? -1
+                  let end = parsedLocString.end ?? -1
+                  if (start === -1 || end === -1) {
+                      alert('Location lacks a start or end, cannot use: ' + option.label)
+                      return null
+                  }
+
+                  // ParsedLocation.start is 0-based
+                  onSelect(parsedLocString.refName + ":" + (1 + parsedLocString.start) + ".." + parsedLocString.end)
+              }
               else {
-                  console.error('No location found for search result')
+                alert('No location found for: ' + option.label)
               }
           }}
           TextFieldProps={{
