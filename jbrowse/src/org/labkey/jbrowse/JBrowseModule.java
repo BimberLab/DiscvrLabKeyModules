ecaee0ee7be24afbbf64ef41b1fa6a18a0872091
bbimber <bbimber@gmail.com>
2023-08-21 09:33:48 -0700

    Rename class

--- jbrowse/src/org/labkey/jbrowse/JBrowseModule.java
+++ jbrowse/src/org/labkey/jbrowse/JBrowseModule.java
@@ -79,7 +79,7 @@ public class JBrowseModule extends ExtendedSimpleModule
     {
         addController(JBrowseController.NAME, JBrowseController.class);
         JBrowseService.setInstance(JBrowseServiceImpl.get());
-        JBrowseService.get().registerFieldCustomizer(new JBrowseLuceneSearch.DefaultJBrowseCustomizer());
+        JBrowseService.get().registerFieldCustomizer(new JBrowseLuceneSearch.DefaultJBrowseFieldCustomizer());
         JBrowseService.get().registerGroupsProvider(new JBrowseLuceneSearch.TestJBrowseGroupProvider());
     }
 
