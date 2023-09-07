8602b102b60c0d8bf2d814329c7372e67620a643
bbimber <bbimber@gmail.com>
2023-09-07 13:27:07 -0700

    Add Rlabkey header to RunConga

--- singlecell/resources/chunks/RunConga.R
+++ singlecell/resources/chunks/RunConga.R
@@ -1,3 +1,11 @@
+if (!file.exists('/homeDir/.netrc')) {
+    print(list.files('/homeDir'))
+    stop('Unable to find file: /homeDir/.netrc')
+}
+
+invisible(Rlabkey::labkey.setCurlOptions(NETRC_FILE = '/homeDir/.netrc'))
+Rdiscvr::SetLabKeyDefaults(baseUrl = serverBaseUrl, defaultFolder = defaultLabKeyFolder)
+
 for (datasetId in names(seuratObjects)) {
     printName(datasetId)
     seuratObj <- readRDS(seuratObjects[[datasetId]])
