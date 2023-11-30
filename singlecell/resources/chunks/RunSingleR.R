bd5230e7166a898e9d317ec7a204b19f3d524dd7
bbimber <bbimber@gmail.com>
2023-11-30 12:59:41 -0600

    Support mouse for SingleR (#248)

--- singlecell/resources/chunks/RunSingleR.R
+++ singlecell/resources/chunks/RunSingleR.R
@@ -12,6 +12,13 @@ library(ExperimentHub)
     printName(datasetId)
     seuratObj <- readSeuratRDS(seuratObjects[[datasetId]])
 
+    if (singleRSpecies == 'mouse') {
+       datasets <- 'MouseRNAseqData'
+    }
+    else {
+       print('Using human singleR datasets')
+    }
+
     seuratObj <- bindArgs(CellMembrane::RunSingleR, seuratObj, disallowedArgNames = c('assay'))()
 
     saveData(seuratObj, datasetId)
