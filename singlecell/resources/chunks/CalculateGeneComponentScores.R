94d4e91c5c03532a50004931c308155e238aa389
bbimber <bbimber@gmail.com>
2024-03-23 19:54:21 -0700

    Add logging

--- singlecell/resources/chunks/CalculateGeneComponentScores.R
+++ singlecell/resources/chunks/CalculateGeneComponentScores.R
@@ -3,6 +3,7 @@ for (datasetId in names(seuratObjects)) {
     seuratObj <- readSeuratRDS(seuratObjects[[datasetId]])
 
     for (sc in savedComponent) {
+        logger::log_info(paste0('Processing ', datasetId, ' for ', sc))
         seuratObj <- RIRA::ScoreUsingSavedComponent(seuratObj, componentOrName = sc, fieldName = sc)
     }
 
