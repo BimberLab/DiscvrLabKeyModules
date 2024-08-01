2dddb2f8dfc9cb94f2837983a011b8c9eb88d0b1
bbimber <bbimber@gmail.com>
2024-08-01 11:08:36 -0700

    Update jest.setup.ts

--- jbrowse/src/client/test/jest.setup.ts
+++ jbrowse/src/client/test/jest.setup.ts
@@ -1,5 +1,5 @@
 import 'regenerator-runtime/runtime';
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from 'enzyme-adapter-react-17';
 
 configure({ adapter: new Adapter() });
