ad249d02c6615b90243dc495cc3771199f58dc8a
Sebastian Benjamin <sebastiancbenjamin@gmail.com>
2024-11-11 16:19:14 -0800

    Fix vertical height calculation for correct overflow

--- jbrowse/src/client/JBrowse/Browser/plugins/ExtendedVariantPlugin/ExtendedVariantWidget/ExtendedVariantWidget.tsx
+++ jbrowse/src/client/JBrowse/Browser/plugins/ExtendedVariantPlugin/ExtendedVariantWidget/ExtendedVariantWidget.tsx
@@ -407,7 +407,7 @@ export default jbrowse => {
         feat["INFO"] = null
 
         return (
-                <Paper data-testid="extended-variant-widget" style={{height: '50%', overflowY: 'scroll'}}>
+                <Paper data-testid="extended-variant-widget" style={{height: '100vh', overflowY: 'scroll'}}>
                     {message}
                     <FeatureDetails
                             feature={feat}
