23c3a306152df034638da8874e27913b53ae30b7
bbimber <bbimber@gmail.com>
2024-12-04 09:22:11 -0800

    Improve leidenalg debugging

--- singlecell/resources/chunks/FindClustersAndDimRedux.R
+++ singlecell/resources/chunks/FindClustersAndDimRedux.R
@@ -1,8 +1,18 @@
 if (!reticulate::py_module_available(module = 'leidenalg')) {
     logger::log_warn('python leidenalg not found!')
     logger::log_warn(paste0('Python available: ', reticulate::py_available()))
-    logger::log_warn(reticulate::py_config())
-    logger::log_warn(paste0('installed packages: ', paste0(reticulate::py_list_packages()$package, collapse = ', ')))
+    logger::log_warn('Python config')
+    pyConfig <- reticulate::py_config()
+    for (pn in names(pyConfig)) {
+        logger::log_warn(paste0(pn, ': ', paste0(pyConfig[[pn]]), collapse = ','))
+    }
+
+    logger::log_warn(paste0('pythonpath: ', reticulate::py_config()$pythonpath))
+
+    logger::log_warn('Python packages:')
+    for (pn in reticulate::py_list_packages()$package) {
+        logger::log_warn(pn)
+    }
 }
 
 for (datasetId in names(seuratObjects)) {
