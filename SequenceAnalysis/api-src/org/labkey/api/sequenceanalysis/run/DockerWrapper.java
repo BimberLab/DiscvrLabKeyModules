97a71bb35092bd1fb0a0371c929035210bf2297a
bbimber <bbimber@gmail.com>
2024-12-03 20:09:35 -0800

    Set scripts executable

--- SequenceAnalysis/api-src/org/labkey/api/sequenceanalysis/run/DockerWrapper.java
+++ SequenceAnalysis/api-src/org/labkey/api/sequenceanalysis/run/DockerWrapper.java
@@ -166,6 +166,8 @@ public class DockerWrapper extends AbstractCommandWrapper
             throw new PipelineJobException(e);
         }
 
+        localBashScript.setExecutable(true);
+        dockerBashScript.setExecutable(true);
         execute(Arrays.asList("/bin/bash", localBashScript.getPath()));
     }
 
