13a6719a355aa2d91029eb0763762ae34dcce93b
bbimber <bbimber@gmail.com>
2024-12-04 08:14:10 -0800

    Add debugging is leidenalg not found

--- singlecell/resources/chunks/FindClustersAndDimRedux.R
+++ singlecell/resources/chunks/FindClustersAndDimRedux.R
@@ -1,3 +1,10 @@
+if (!reticulate::py_module_available(module = 'leidenalg')) {
+    logger::log_warn('python leidenalg not found!')
+    logger::log_warn(paste0('Python available: ', reticulate::py_available()))
+    logger::log_warn(reticulate::py_config())
+    logger::log_warn(paste0('installed packages: ', paste0(reticulate::py_list_packages()$package, collapse = ', ')))
+}
+
 for (datasetId in names(seuratObjects)) {
     printName(datasetId)
     seuratObj <- readSeuratRDS(seuratObjects[[datasetId]])
