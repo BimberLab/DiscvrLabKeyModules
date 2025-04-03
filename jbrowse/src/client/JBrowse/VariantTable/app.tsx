e8caa8863d1c67b22654292bf134a16ce8aa61e7
bbimber <bbimber@gmail.com>
2025-04-03 09:39:06 -0700

    Update app/dev.tsx

--- jbrowse/src/client/JBrowse/VariantTable/app.tsx
+++ jbrowse/src/client/JBrowse/VariantTable/app.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import VariantTable from './VariantTable';
 import { createRoot } from 'react-dom/client';
 
-// Need to wait for container element to be available in labkey wrapper before render
 window.addEventListener('DOMContentLoaded', (event) => {
     createRoot(document.getElementById('app')).render(<VariantTable />)
 });
