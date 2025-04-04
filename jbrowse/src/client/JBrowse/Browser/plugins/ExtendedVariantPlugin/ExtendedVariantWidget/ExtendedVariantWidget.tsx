15ff65a66ad2c0b3795e0032548c1edb0501f67e
Sebastian Benjamin <sebastiancbenjamin@gmail.com>
2025-04-04 14:33:22 -0700

    Remove seperate overflow control from ExtendedVariantWidget

--- jbrowse/src/client/JBrowse/Browser/plugins/ExtendedVariantPlugin/ExtendedVariantWidget/ExtendedVariantWidget.tsx
+++ jbrowse/src/client/JBrowse/Browser/plugins/ExtendedVariantPlugin/ExtendedVariantWidget/ExtendedVariantWidget.tsx
@@ -407,7 +407,7 @@ export default jbrowse => {
         feat["INFO"] = null
 
         return (
-                <Paper data-testid="extended-variant-widget" style={{height: '100vh', overflowY: 'scroll'}}>
+                <Paper data-testid="extended-variant-widget">
                     {message}
                     <FeatureDetails
                             feature={feat}
