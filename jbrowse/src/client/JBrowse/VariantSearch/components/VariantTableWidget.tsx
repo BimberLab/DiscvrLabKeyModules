aa496fde13df8ac921c39aa171e776997f8ca824
Sebastian Benjamin <sebastiancbenjamin@gmail.com>
2025-04-04 15:34:40 -0700

    Add share button to VariantTable

--- jbrowse/src/client/JBrowse/VariantSearch/components/VariantTableWidget.tsx
+++ jbrowse/src/client/JBrowse/VariantSearch/components/VariantTableWidget.tsx
@@ -13,7 +13,6 @@ import {
     GridToolbarExport
 } from '@mui/x-data-grid';
 import SearchIcon from '@mui/icons-material/Search';
-import LinkIcon from '@mui/icons-material/Link';
 import React, { useEffect, useState } from 'react';
 import { getConf } from '@jbrowse/core/configuration';
 import { AppBar, Box, Button, Dialog, Paper, Popover, Toolbar, Tooltip, Typography } from '@mui/material';
@@ -37,6 +36,7 @@ import '../../jbrowse.css';
 import LoadingIndicator from './LoadingIndicator';
 import { BaseFeatureDataAdapter } from '@jbrowse/core/data_adapters/BaseAdapter';
 import { lastValueFrom } from 'rxjs';
+import { ShareButton } from  './ShareButton';
 
 const VariantTableWidget = observer(props => {
     const numberFormatter = new Intl.NumberFormat('en-US');
@@ -201,25 +201,7 @@ const VariantTableWidget = observer(props => {
                     delimiter: ';',
                 }} />
 
-                <Button
-                    startIcon={<LinkIcon />}
-                    size="small"
-                    color="primary"
-                    onClick={() => {
-                        navigator.clipboard.writeText(window.location.href)
-                        .then(() => {
-                            // Popup message for successful copy
-                            alert('URL copied to clipboard.');
-                        })
-                        .catch(err => {
-                            // Error handling
-                            console.error('Failed to copy the URL: ', err);
-                            alert('Failed to copy the URL.');
-                        });
-                    }}
-                >
-                Share
-                </Button>
+                <ShareButton />
             </GridToolbarContainer>
         );
     }
