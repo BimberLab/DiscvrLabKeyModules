932e990d29665c468431087fd5b3b16f493dc495
bbimber <bbimber@gmail.com>
2025-05-06 13:12:26 -0700

    Pass nCores to UCell

--- singlecell/resources/chunks/CalculateUCellScores.R
+++ singlecell/resources/chunks/CalculateUCellScores.R
@@ -1,9 +1,15 @@
+if (Sys.getenv('SEURAT_MAX_THREADS') != '') {
+    nCores <- Sys.getenv('SEURAT_MAX_THREADS')
+} else {
+    nCores <- 1
+}
+
 for (datasetId in names(seuratObjects)) {
     printName(datasetId)
     seuratObj <- readSeuratRDS(seuratObjects[[datasetId]])
 
     message(paste0('Loading dataset ', datasetId, ', with total cells: ', ncol(seuratObj)))
-    seuratObj <- RIRA::CalculateUCellScores(seuratObj, storeRanks = storeRanks, assayName = assayName, forceRecalculate = forceRecalculate)
+    seuratObj <- RIRA::CalculateUCellScores(seuratObj, storeRanks = storeRanks, assayName = assayName, forceRecalculate = forceRecalculate, ncores = nCores)
 
     saveData(seuratObj, datasetId)
 
