56c5fc4a68c1bd91fc13e07c991ed4bfe703f495
bbimber <bbimber@gmail.com>
2025-08-05 15:32:28 -0700

    Allow greater curl timeout

--- singlecell/resources/chunks/ApplyKnownClonotypicData.R
+++ singlecell/resources/chunks/ApplyKnownClonotypicData.R
@@ -4,7 +4,7 @@ if (!file.exists(netRc)) {
     stop(paste0('Unable to find file: ', netRc))
 }
 
-invisible(Rlabkey::labkey.setCurlOptions(NETRC_FILE = netRc, connect-timeout = 10))
+invisible(Rlabkey::labkey.setCurlOptions(NETRC_FILE = netRc, 'connect-timeout' = 60))
 Rdiscvr::SetLabKeyDefaults(baseUrl = serverBaseUrl, defaultFolder = defaultLabKeyFolder)
 
 for (datasetId in names(seuratObjects)) {
