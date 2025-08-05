8d630fd64118d7033c36cfc44839281919443219
bbimber <bbimber@gmail.com>
2025-08-05 11:05:50 -0700

    Update FastqcRunner.java

--- SequenceAnalysis/src/org/labkey/sequenceanalysis/run/util/FastqcRunner.java
+++ SequenceAnalysis/src/org/labkey/sequenceanalysis/run/util/FastqcRunner.java
@@ -46,6 +46,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
