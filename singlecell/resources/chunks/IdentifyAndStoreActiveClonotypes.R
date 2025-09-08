f005c7667e0a196b2602e520a3a43f39a27c0dc7
bbimber <bbimber@gmail.com>
2025-09-07 18:26:50 -0700

    Do not storeStimLevelData for TRA

--- singlecell/resources/chunks/IdentifyAndStoreActiveClonotypes.R
+++ singlecell/resources/chunks/IdentifyAndStoreActiveClonotypes.R
@@ -11,7 +11,7 @@ for (datasetId in names(seuratObjects)) {
   printName(datasetId)
   seuratObj <- readSeuratRDS(seuratObjects[[datasetId]])
 
-  Rdiscvr::IdentifyAndStoreActiveClonotypes(seuratObj, chain = 'TRA')
+  Rdiscvr::IdentifyAndStoreActiveClonotypes(seuratObj, chain = 'TRA', storeStimLevelData = FALSE)
   Rdiscvr::IdentifyAndStoreActiveClonotypes(seuratObj, chain = 'TRB')
 
   saveData(seuratObj, datasetId)
