8dfb9269650068cd42bbcbac3c21698545cee5e9
bbimber <bbimber@gmail.com>
2025-12-06 17:40:21 -0800

    Initial support for tcrClustR

---
+++ singlecell/resources/chunks/PerformTcrClustering.R
@@ -0,0 +1,23 @@
+for (datasetId in names(seuratObjects)) {
+    printName(datasetId)
+    seuratObj <- readSeuratRDS(seuratObjects[[datasetId]])
+
+    seuratObj <- tcrClustR::CalculateTcrDistances(
+      inputData = seuratObj,
+      chains = c('TRA', 'TRB'),
+      minimumCloneSize = 2,
+      calculateChainPairs = TRUE
+    )
+
+    seuratObj <- tcrClustR::RunTcrClustering(
+      seuratObj_TCR = seuratObj,
+      dianaHeight = 20,
+      clusterSizeThreshold = 1
+    )
+
+    saveData(seuratObj, datasetId)
+
+    # Cleanup
+    rm(seuratObj)
+    gc()
+}
---
+++ singlecell/src/org/labkey/singlecell/pipeline/singlecell/AbstractTcrClustRStep.java
@@ -0,0 +1,37 @@
+package org.labkey.singlecell.pipeline.singlecell;
+
+import org.labkey.api.sequenceanalysis.pipeline.PipelineContext;
+import org.labkey.api.sequenceanalysis.pipeline.PipelineStepProvider;
+import org.labkey.api.singlecell.pipeline.AbstractSingleCellPipelineStep;
+import org.labkey.api.util.PageFlowUtil;
+
+import java.util.Collection;
+
+abstract public class AbstractTcrClustRStep extends AbstractSingleCellPipelineStep
+{
+    public static String CONTAINER_NAME = "ghcr.io/bimberlabinternal/tcrclustr:latest";
+
+    public AbstractTcrClustRStep(PipelineStepProvider<?> provider, PipelineContext ctx)
+    {
+        super(provider, ctx);
+    }
+
+    @Override
+    public Collection<String> getRLibraries()
+    {
+        return PageFlowUtil.set("tcrClustR");
+    }
+
+    @Override
+    public String getDockerContainerName()
+    {
+        return CONTAINER_NAME;
+    }
+
+    // NOTE: ExperimentHub and similar packages default to saving data to the user's home dir. Set a directory, to avoid issues when not running the container as root
+    @Override
+    public String getDockerHomeDir()
+    {
+        return "/dockerHomeDir";
+    }
+}
