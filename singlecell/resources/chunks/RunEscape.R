2950346dec865c3c489d69716ce059e49f08d33a
bbimber <bbimber@gmail.com>
2025-12-25 08:27:49 -0800

    Better resume behavior for escape

--- singlecell/resources/chunks/RunEscape.R
+++ singlecell/resources/chunks/RunEscape.R
@@ -5,14 +5,41 @@ if (Sys.getenv('SEURAT_MAX_THREADS') != '') {
 }
 
 for (datasetId in names(seuratObjects)) {
-    printName(datasetId)
-    seuratObj <- readSeuratRDS(seuratObjects[[datasetId]])
+  printName(datasetId)
+  seuratObj <- readSeuratRDS(seuratObjects[[datasetId]])
 
-    seuratObj <- CellMembrane::RunEscape(seuratObj, outputAssayBaseName = outputAssayBaseName, doPlot = TRUE, heatmapGroupingVars = heatmapGroupingVars, performDimRedux = performDimRedux, escapeMethod = escapeMethod, nCores = nCores)
+  toDelete <- c()
 
-    saveData(seuratObj, datasetId)
+  vals <- eval(formals(CellMembrane::RunEscape)$msigdbGeneSets)
+  for (idx in seq_along(vals)) {
+    geneSetName <- names(vals)[idx]
+    geneSet <- vals[[idx]]
+    logger::log_info(paste0('Processing: ', geneSetName, ' / ', geneSet))
 
-    # Cleanup
-    rm(seuratObj)
-    gc()
+    fn <- paste0('escape.', datasetId, '.', ifelse(geneSetName == '', yes = geneSet, no = geneSetName), '.rds')
+    if (file.exists(fn)) {
+      logger::log_info(paste0('resuming: ', fn))
+      seuratObj <- readRDS(fn)
+      toDelete <- c(toDelete, fn)
+    } else {
+      msigdbGeneSets <- geneSet
+      if (geneSetName != '') {
+        names(msigdbGeneSets) <- geneSetName
+      }
+
+      seuratObj <- CellMembrane::RunEscape(seuratObj, msigdbGeneSets = msigdbGeneSets, outputAssayBaseName = outputAssayBaseName, doPlot = TRUE, heatmapGroupingVars = heatmapGroupingVars, performDimRedux = performDimRedux, escapeMethod = escapeMethod, nCores = nCores)
+      saveRDS(seuratObj, file = fn)
+      toDelete <- c(toDelete, fn)
+    }
+  }
+
+  for(fn in toDelete) {
+    unlink(fn)
+  }
+
+  saveData(seuratObj, datasetId)
+
+  # Cleanup
+  rm(seuratObj)
+  gc()
 }
