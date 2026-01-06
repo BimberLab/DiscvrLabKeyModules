55a13368abc145fa426fe7745c7dc7d3fa6933ed
bbimber <bbimber@gmail.com>
2026-01-06 05:53:41 -0800

    Improve logging

--- SequenceAnalysis/src/org/labkey/sequenceanalysis/SequenceAnalysisController.java
+++ SequenceAnalysis/src/org/labkey/sequenceanalysis/SequenceAnalysisController.java
@@ -1191,7 +1191,7 @@ public class SequenceAnalysisController extends SpringActionController
             {
                 ExceptionUtil.logExceptionToMothership(getViewContext().getRequest(), e);
                 errors.reject(ERROR_MSG, e.getMessage());
-                _log.error(e);
+                _log.error(e.getMessage(), e);
                 return null;
             }
             return new ApiSimpleResponse(resultProperties);
@@ -4821,7 +4821,7 @@ public class SequenceAnalysisController extends SpringActionController
                 catch (IOException e)
                 {
                     errors.reject(ERROR_MSG, e.getMessage());
-                    _log.error(e);
+                    _log.error(e.getMessage(), e);
 
                     return null;
                 }
@@ -4837,7 +4837,7 @@ public class SequenceAnalysisController extends SpringActionController
             catch (PipelineValidationException e)
             {
                 errors.reject(ERROR_MSG, e.getMessage());
-                _log.error(e);
+                _log.error(e.getMessage(), e);
 
                 return null;
             }
