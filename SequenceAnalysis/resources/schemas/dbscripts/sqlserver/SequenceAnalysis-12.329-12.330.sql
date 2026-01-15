914ec467a350d16d875a0ef1ea58e9d67204b36a
bbimber <bbimber@gmail.com>
2026-01-15 10:18:40 -0800

    AUTO_DROP not supported prior to MSSql 2022

--- SequenceAnalysis/resources/schemas/dbscripts/sqlserver/SequenceAnalysis-12.329-12.330.sql
+++ SequenceAnalysis/resources/schemas/dbscripts/sqlserver/SequenceAnalysis-12.329-12.330.sql
@@ -17,23 +17,17 @@ CREATE NONCLUSTERED INDEX IDX_alignment_summary_analysis_id_rowid_container_tota
 	rowid ASC,
 	container ASC
 )
-INCLUDE(total)
+INCLUDE(total);
 
-CREATE STATISTICS STAT_ref_nt_sequence_rowid_locus_container ON sequenceanalysis.ref_nt_sequences (RowId, locus, container)
-WITH AUTO_DROP = OFF
+CREATE STATISTICS STAT_ref_nt_sequence_rowid_locus_container ON sequenceanalysis.ref_nt_sequences (RowId, locus, container);
 
-CREATE STATISTICS STAT_ref_nt_sequence_locus_container ON sequenceanalysis.ref_nt_sequences (locus, container)
-WITH AUTO_DROP = OFF
+CREATE STATISTICS STAT_ref_nt_sequence_locus_container ON sequenceanalysis.ref_nt_sequences (locus, container);
 
-CREATE STATISTICS STAT_sequence_analyses_container_readset ON sequenceanalysis.sequence_analyses (Container, readset)
-WITH AUTO_DROP = OFF
+CREATE STATISTICS STAT_sequence_analyses_container_readset ON sequenceanalysis.sequence_analyses (Container, readset);
 
-CREATE STATISTICS STAT_sequence_readsets_rowid_container ON sequenceanalysis.sequence_readsets (RowId, Container)
-WITH AUTO_DROP = OFF
+CREATE STATISTICS STAT_sequence_readsets_rowid_container ON sequenceanalysis.sequence_readsets (RowId, Container);
 
-CREATE STATISTICS STAT_asj_alignmentid_container_ref_nt_id ON sequenceanalysis.alignment_summary_junction (alignment_id, container, ref_nt_id)
-WITH AUTO_DROP = OFF
+CREATE STATISTICS STAT_asj_alignmentid_container_ref_nt_id ON sequenceanalysis.alignment_summary_junction (alignment_id, container, ref_nt_id);
               
-CREATE STATISTICS STAT_asj_alignmentid_ref_nt_id_status_alignment_id ON sequenceanalysis.alignment_summary_junction (ref_nt_id, status, alignment_id)
-WITH AUTO_DROP = OFF
+CREATE STATISTICS STAT_asj_alignmentid_ref_nt_id_status_alignment_id ON sequenceanalysis.alignment_summary_junction (ref_nt_id, status, alignment_id);
 
