ec71a72bd4093eb53a8e0db1baebfbd7c9f7d442
bbimber <bbimber@gmail.com>
2026-01-15 23:16:36 -0800

    Use query parameters in alignment_summary queries

--- SequenceAnalysis/resources/queries/sequenceanalysis/alignment_summary_by_lineage_pivoted.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  a.analysis_id,
-  a.lineages,
-  sum(a.percent) as percent
-
-FROM sequenceanalysis.alignment_summary_by_lineage a
-
-GROUP BY a.analysis_id, a.lineages
-PIVOT percent BY lineages
