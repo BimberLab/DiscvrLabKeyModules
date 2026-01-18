8dad8955f5844bd599f407691e7498160dbbf6d2
bbimber <bbimber@gmail.com>
2026-01-18 07:42:04 -0800

    Set baseUrl in PredictTcellActivation.R

--- singlecell/resources/chunks/PredictTcellActivation.R
+++ singlecell/resources/chunks/PredictTcellActivation.R
@@ -1,3 +1,12 @@
+netRc <- paste0(Sys.getenv('USER_HOME'), '/.netrc')
+if (!file.exists(netRc)) {
+  print(list.files(Sys.getenv('USER_HOME')))
+  stop(paste0('Unable to find file: ', netRc))
+}
+
+invisible(Rlabkey::labkey.setCurlOptions(NETRC_FILE = netRc))
+Rdiscvr::SetLabKeyDefaults(baseUrl = serverBaseUrl, defaultFolder = defaultLabKeyFolder)
+
 for (datasetId in names(seuratObjects)) {
   printName(datasetId)
   seuratObj <- readSeuratRDS(seuratObjects[[datasetId]])
