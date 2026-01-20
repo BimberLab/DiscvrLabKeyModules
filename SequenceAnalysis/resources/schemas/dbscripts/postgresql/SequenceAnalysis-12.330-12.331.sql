c060d953a55e0dd069343f7b60153920fda97342
bbimber <bbimber@gmail.com>
2026-01-20 11:02:34 -0800

    Remove sequenceanalysis indexes (#373)

---
+++ SequenceAnalysis/resources/schemas/dbscripts/postgresql/SequenceAnalysis-12.330-12.331.sql
@@ -0,0 +1,5 @@
+-- This is a reversal of SequenceAnalysis-12.329-12.330.sql:
+DROP INDEX IF EXISTS sequenceanalysis.IDX_asj_status_container_alignment_id_ref_nt_id;
+DROP INDEX IF EXISTS sequenceanalysis.IDX_haplotypes_name_date;
+DROP INDEX IF EXISTS sequenceanalysis.IDX_haplotype_sequences_name_haplotype_type;
+DROP INDEX IF EXISTS sequenceanalysis.IDX_alignment_summary_analysis_id_rowid_container_total;
