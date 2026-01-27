035009d9c3b6f0997dba699823ef94fe852fd206
bbimber <bbimber@gmail.com>
2026-01-27 10:05:07 -0800

    Support new gene component

--- singlecell/src/org/labkey/singlecell/pipeline/singlecell/CalculateGeneComponentScores.java
+++ singlecell/src/org/labkey/singlecell/pipeline/singlecell/CalculateGeneComponentScores.java
@@ -7,8 +7,8 @@ import org.labkey.api.singlecell.pipeline.SeuratToolParameter;
 import org.labkey.api.singlecell.pipeline.SingleCellStep;
 import org.labkey.api.util.PageFlowUtil;
 
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 
 public class CalculateGeneComponentScores extends AbstractRiraStep
 {
@@ -21,16 +21,16 @@ public class CalculateGeneComponentScores extends AbstractRiraStep
     {
         public Provider()
         {
-            super("CalculateGeneComponentScores", "Calculate Gene Module Scores", "RIRA", "This will generate UCell scores for a set of pre-defined gene modules", Collections.singletonList(
+            super("CalculateGeneComponentScores", "Calculate Gene Module Scores", "RIRA", "This will generate UCell scores for a set of pre-defined gene modules", Arrays.asList(
                     SeuratToolParameter.create("savedComponent", "Saved Component(s)", "This is the name of the saved component (from RIRA) to apply", "ldk-simplecombo", new JSONObject()
                     {{
-                        put("storeValues", "Tcell_EffectorDifferentiation;TCR_EarlyStimulationComponent;CD4_Activation_Axis");
+                        put("storeValues", "Tcell_EffectorDifferentiation;TCR_EarlyStimulationComponent;CD4_Activation_Axis;PRF1_EffectorAxis");
                         put("multiSelect", true);
                         put("allowBlank", false);
                         put("joinReturnValue", true);
                         put("delimiter", ";");
-
-                    }}, "Tcell_EffectorDifferentiation", null, true, true).delimiter(";")
+                    }}, "Tcell_EffectorDifferentiation", null, true, true).delimiter(";"),
+                    SeuratToolParameter.create("useScaledData", "Use scale.data", "By default, these are calculated against the data slot. If checked, it will use scale.data instead", "checkbox", null, false)
             ), null, null);
         }
 
