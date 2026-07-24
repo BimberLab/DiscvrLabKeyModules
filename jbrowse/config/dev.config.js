cd10e6ed07e7be049fe9553e24a1cd0acb6be0cd
bbimber <bbimber@gmail.com>
2026-07-24 11:35:13 -0700

    Upgrade to @labkey/build 10.x

--- jbrowse/config/dev.config.js
+++ jbrowse/config/dev.config.js
@@ -4,20 +4,24 @@
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
 
-const devConfig = require('../node_modules/@labkey/build/webpack/dev.config')
+const devConfig = require('../node_modules/@labkey/build/config/dev.config')
 
 const entryPoints = require('../src/client/entryPoints');
-const constants = require('../node_modules/@labkey/build/webpack/constants');
+const constants = require('../node_modules/@labkey/build/config/constants');
+
+// TODO: this is probably not needed
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 
 const clientConfig = devConfig
 
+// TODO: this is probably not needed
 // See: https://stackoverflow.com/questions/68707553/uncaught-referenceerror-buffer-is-not-defined
 clientConfig.resolve.fallback =
 {
     "buffer": require.resolve("buffer")
 }
 
+// TODO: this is probably not needed
 clientConfig.plugins = [new NodePolyfillPlugin()].concat(constants.processPlugins(entryPoints))
 
 clientConfig.output.publicPath = 'auto'
--- jbrowse/config/prod.config.js
+++ jbrowse/config/prod.config.js
@@ -4,14 +4,17 @@
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
 
-const prodConfig = require('../node_modules/@labkey/build/webpack/prod.config')
+const prodConfig = require('../node_modules/@labkey/build/config/prod.config')
 
 const entryPoints = require('../src/client/entryPoints');
-const constants = require('../node_modules/@labkey/build/webpack/constants');
+const constants = require('../node_modules/@labkey/build/config/constants');
+
+// TODO: this is probably no longer required
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 
 const clientConfig = prodConfig
 
+// TODO: this is probably no longer required
 // See: https://stackoverflow.com/questions/68707553/uncaught-referenceerror-buffer-is-not-defined
 clientConfig.resolve.fallback =
 {
@@ -23,6 +26,7 @@ clientConfig.resolve.fallback =
     "buffer": require.resolve("buffer")
 }
 
+// TODO: this might also not be needed
 clientConfig.plugins = [new NodePolyfillPlugin()].concat(constants.processPlugins(entryPoints))
 
 clientConfig.output.publicPath = 'auto'
